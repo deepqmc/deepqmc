752e411a486c33c36a78d96ceda63fbff081454b
zenoone <zenoone@physik.fu-berlin.de>
2018-11-22 10:31:35 +0100

    yo

--- Hydrogen23D.py
+++ Hydrogen23D.py
@@ -19,12 +19,11 @@ qe  = const.e
 e_0  = const.epsilon_0
 ev  = const.electron_volt
 
-
 class Net(nn.Module):
 	def __init__(self):
 		super(Net, self).__init__()
 		self.NN=nn.Sequential(
-				torch.nn.Linear(3, 64),
+				torch.nn.Linear(6, 64),
 				torch.nn.ReLU(),
 				torch.nn.Linear(64, 64),
 				torch.nn.ReLU(),
@@ -33,28 +32,28 @@ class Net(nn.Module):
 				torch.nn.Linear(64, 1)
 				)
 		self.Lambda=nn.Parameter(torch.Tensor([0]))#eigenvalue
-		self.alpha=nn.Parameter(torch.Tensor([2]))#coefficient for decay
-		self.beta=nn.Parameter(torch.Tensor([2]))#coefficient for decay
+		self.alpha=nn.Parameter(torch.Tensor([1]))#coefficient for decay
+		self.beta=nn.Parameter(torch.Tensor([1]))#coefficient for decay
 
 	def forward(self,x):
 		return self.NN(x)[:,0]*torch.exp(-F.softplus(torch.abs(self.alpha*torch.norm(x,dim=1))-self.beta))
 
 
-LR=1e-3
-BATCH_SIZE=128
-H=1
+LR=3e-3
+BATCH_SIZE=256
+H=0.1
 
 net = Net()
 params = [p for p in net.parameters()]
 opt = torch.optim.Adam(params, lr=LR)
 
-steps=500
+steps=5000
 for epoch in range(5):
 	start = time.time()
 	for step in range(steps):
 		print("Progress {:2.0%}".format(step /steps), end="\r")
-		X = (torch.rand(BATCH_SIZE,3,requires_grad=True)-0.5)*2
-		X = X/torch.norm(X,dim=1)[:,None]*(torch.rand(BATCH_SIZE)[:,None]*2+0.001)
+		X = (torch.rand(BATCH_SIZE,6,requires_grad=True)-0.5)*2
+		X = X/torch.norm(X,dim=1)[:,None]*(torch.rand(BATCH_SIZE)[:,None]*5)
 		#X = ((torch.rand(BATCH_SIZE,3,requires_grad=True))*5+0.01)
 		eps_0 = torch.from_numpy(np.random.normal(0,H,X.shape)).type(torch.FloatTensor)
 		eps_1 = torch.from_numpy(np.random.normal(0,H,X.shape)).type(torch.FloatTensor)
@@ -68,36 +67,44 @@ for epoch in range(5):
 		Psi_1_n = net(X-eps_1)
 		Psi_1   = (Psi_1_p + Psi_1_n)/2
 
-		Lap_0 = (hb*10**12)**2/(2*mu)*eps_0*(grad(Psi_0_p,X,create_graph=True,grad_outputs=torch.ones_like(Psi_0_p))[0]-grad(Psi_0_n,X,create_graph=True,grad_outputs=torch.ones_like(Psi_1_p))[0])/(4*H**2)
-		Lap_1 = (hb*10**12)**2/(2*mu)*eps_1*(grad(Psi_1_p,X,create_graph=True,grad_outputs=torch.ones_like(Psi_0_n))[0]-grad(Psi_1_n,X,create_graph=True,grad_outputs=torch.ones_like(Psi_1_n))[0])/(4*H**2)
+		Lap_0 = a_0**2/2*eps_0*(grad(Psi_0_p,X,create_graph=True,grad_outputs=torch.ones_like(Psi_0_p))[0]-grad(Psi_0_n,X,create_graph=True,grad_outputs=torch.ones_like(Psi_1_p))[0])/(4*H**2)
+		Lap_1 = a_0**2/2*eps_1*(grad(Psi_1_p,X,create_graph=True,grad_outputs=torch.ones_like(Psi_0_n))[0]-grad(Psi_1_n,X,create_graph=True,grad_outputs=torch.ones_like(Psi_1_n))[0])/(4*H**2)
 
 		Lap_0 = torch.sum(Lap_0,dim=1)
 		Lap_1 = torch.sum(Lap_1,dim=1)
-		r     = torch.norm(X,dim=1)
-		V     = - 1/r * qe**2/(4*pi*e_0)*10**12
+		#r     = torch.norm(X,dim=1)
+		R1     = torch.tensor([2,0,0]).type(torch.FloatTensor)
+		R2     = torch.tensor([-2,0,0]).type(torch.FloatTensor)
+		r11    = torch.norm(X[:,:3]-R1,dim=1)
+		r12    = torch.norm(X[:,:3]-R2,dim=1)
+		r21    = torch.norm(X[:,3:]-R1,dim=1)
+		r22    = torch.norm(X[:,3:]-R2,dim=1)
+		rinter = torch.norm(X[:,:3]-X[:,3:],dim=1)
 
-		J     = torch.mean((-Lap_0 + (V-net.Lambda)*Psi_0)/qe*(-Lap_1+ (V-net.Lambda)*Psi_1)/qe/(Psi_0*Psi_1))
-		print("_____________________________________")
-		print(V/qe)
-		print(torch.mean((-Lap_0 + (V-net.Lambda)*Psi_0)/qe).item())
+
+		V     = - (1/r11+1/r12+1/r21+1/r22) + (1/rinter)
+
+		J     = torch.mean((-Lap_0 + (V-net.Lambda)*Psi_0)*(-Lap_1+ (V-net.Lambda)*Psi_1)/(Psi_0*Psi_1))
 
 		opt.zero_grad()
 		J.backward()
 		opt.step()
+
 
 	print('e # '+str(epoch+1)+'_____________________________________')
 	print('It took', time.time()-start, 'seconds.')
 	print('Lambda = '+str(net.Lambda[0].item()))
 	print('Alpha  = '+str(net.alpha[0].item()))
 	print('Beta   = '+str(net.beta[0].item()))
+	#print("E = ",(net.Lambda[0].item())*mu/hb**2/ev)
 
-	X_plot = (torch.rand(50000,3,requires_grad=True)-0.5)*2
-	X_plot = X_plot/torch.norm(X_plot,dim=1)[:,None]*(torch.rand(50000)[:,None]*2)
+	X_plot = (torch.rand(5000,6,requires_grad=True)-0.5)*2
+	X_plot = X_plot/torch.norm(X_plot,dim=1)[:,None]*(torch.rand(5000)[:,None]*5)
 	X_plot[:,1] = 0
 	X_plot[:,2] = 0
 
 	Psi_plot = net(X_plot).detach().numpy()
-	R_plot = np.linalg.norm(X_plot.detach().numpy(),axis=1)*((X_plot[:,0].detach().numpy()>0).astype(int) - (X_plot[:,0].detach().numpy()<=0).astype(int))
-	plt.plot(R_plot,np.abs((Psi_plot)/max(np.abs(Psi_plot))),marker='.',ls='',ms=1.5,label=(str(epoch)))#+"  e = "+str(energy)+"  l = "+str(net.Lambda.item())))
+	R_plot = np.linalg.norm(X_plot[:,:3].detach().numpy(),axis=1)*((X_plot[:,3].detach().numpy()>0).astype(int) - (X_plot[:,3].detach().numpy()<=0).astype(int))
+	plt.plot(R_plot,(Psi_plot)/max(np.abs(Psi_plot)),marker='.',ls='',ms=1.5,label=(str(epoch)))#+"  energy = "+str(energy)))
 plt.legend(loc="upper right")
 plt.show()
--- smoothnet.py
+++ smoothnet.py
@@ -12,20 +12,21 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.NN=nn.Sequential(
-                torch.nn.Linear(1, 64),
+                torch.nn.Linear(1, 16),
+                # torch.nn.ReLU(),
+                # torch.nn.Linear(64, 64),
+                # torch.nn.ReLU(),
+                # torch.nn.Linear(64, 64),
                 torch.nn.ReLU(),
-                torch.nn.Linear(64, 64),
-                torch.nn.ReLU(),
-                torch.nn.Linear(64, 64),
-                torch.nn.ReLU(),
-                torch.nn.Linear(64, 1)
+                torch.nn.Linear(16, 1)
                 )
         self.Lambda=nn.Parameter(torch.Tensor([0]))#eigenvalue
         self.alpha=nn.Parameter(torch.Tensor([1.]))#coefficient for decay
         self.beta=nn.Parameter(torch.Tensor([1.]))#coefficient for decay
 
-    def forward(self,x,n=10):
-        return self.NN(x)#*torch.exp(-F.softplus(torch.abs(self.alpha*x)-self.beta))
+    def forward(self,x):
+        return self.NN(x)*((x>-4)*(x<4)).type(torch.FloatTensor)
+        #return self.NN(x)*torch.exp(-F.softplus(torch.abs(self.alpha*x)-self.beta))
 
 
 def analytical_gs(x):
@@ -48,12 +49,12 @@ def save_psi(net,x_range=(-5,5,1000)):
     return Psi
 
 
-LR=1e-3
-BATCH_SIZE=128
+LR=3e-3
+BATCH_SIZE=256#128
 H = 0.1
-v0 = 4
+v0 = 2
 L = 2
-steps = 15000
+steps = 1500
 
 
 
@@ -62,18 +63,22 @@ plotlist=[] # initialise plotlist
 #plotlist.append(make_plot(net_all)) #append untrained network
 #net = copy.deepcopy(net_all)	 # in case I want to train with same initial network
 nm=1
-Hlist=[0.1,0.05,0.01,0.005]
+#Hlist=[0.1,0.05,0.01,0.005]
+Hlist=[0.05]
 for H in Hlist:
     net = copy.deepcopy(net_all)
     params = [p for p in net.parameters()]
     #del params[0]
     opt = torch.optim.Adam(params, lr=LR)
-    for epochs in range(1):
+    for epochs in range(5):
         start = time.time()
         for step in range(steps):
             print("Progress {:2.0%}".format(step /steps), end="\r")
 
             X     = ((torch.rand(BATCH_SIZE,1,requires_grad=True))-0.5)*4*L
+            X[0] = torch.tensor([-4])
+            X[-1] = torch.tensor([4])
+
             X     = X.repeat(1,nm).view(-1,1)
             eps_0 = torch.from_numpy(np.random.normal(0,H,len(X))).type(torch.FloatTensor).view(-1,1)
             eps_1 = torch.from_numpy(np.random.normal(0,H,len(X))).type(torch.FloatTensor).view(-1,1)
@@ -111,11 +116,11 @@ for H in Hlist:
 plt.figure(figsize=(12,8))
 x_plot=np.linspace(-5,5,1000)
 for i,Psi in enumerate(plotlist):
-    #if not (i+1)==len(plotlist):
-        #plt.plot(x_plot,Psi,label="Episode: "+str(i),ls=':',linewidth=1.,color='grey')
-    plt.plot(x_plot,Psi,ls='-',linewidth=2.,label=Hlist[i])
-    #else:
-    #    plt.plot(x_plot,Psi,label="Episode: "+str(i))
+    if not (i+1)==len(plotlist):
+        plt.plot(x_plot,Psi,label="Episode: "+str(i),ls=':',linewidth=1.)#,color='grey')
+    #plt.plot(x_plot,Psi,ls='-',linewidth=2.,label=Hlist[i])
+    else:
+        plt.plot(x_plot,Psi,label="Episode: "+str(i))
 #Psi_mean=np.mean(np.array(plotlist),axis=0)
 #Psi_std=np.std(np.array(plotlist),axis=0)
 plt.plot(x_plot,analytical_gs(x_plot),label='True WF',color='k')
