c6040eb1fe4cc1071b9a900b775061a3449d10b7
zenoone <zenoone@physik.fu-berlin.de>
2018-12-13 11:14:53 +0100

    update

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from Combined import *
+from ELU import *
 
 E_var1=[]
 E_var2=[]
@@ -10,22 +10,15 @@ E_en3=[]
 
 E_varen=[]
 
-dist = [0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0]
+#dist = [0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0]
+dist = [1.0]
 
 for R in dist:
-    E_en1.append(fit(batch_size=2000,steps=5000,epochs=10,losses=["energy"],R1=R,R2=-R)[1])
-    E_en2.append(fit(batch_size=500,steps=10000,epochs=10,losses=["energy"],R1=R,R2=-R)[1])
-    E_en3.append(fit(batch_size=100,steps=20000,epochs=10,losses=["energy"],R1=R,R2=-R)[1])
+    E_en1.append(fit(batch_size=2000,steps=5000,epochs=10,losses=["energy","symmetry","energy"],R1=R,R2=-R)[1])
     #E_en.append(fit(steps=2000,epochs=20,losses=["energy"],R1=R,R2=-R)[1])
 
-plt.figure()
-plt.plot(dist,E_var1,label="bs2000")
-plt.plot(dist,E_var2,label="bs500")
-plt.plot(dist,E_var3,label="bs100")
-#plt.plot(dist,E_en)
-plt.savefig("testlosses0912en.png")
-E_var=np.mean(np.array([E_var1,E_var2,E_var3]),axis=0)
-plt.plot(dist,E_var)
-plt.savefig("testlossesmean0912en.png")
+#plt.figure()
+#plt.plot(dist,E_en1)
+#plt.savefig("testlosses0912en.png")
 
-#plt.show()
+plt.show()
--- test.py
+++ test.py
@@ -11,6 +11,100 @@ import torch.nn.functional as F
 from matplotlib import cm
 from Combined import *
 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.NN=nn.Sequential(
+                torch.nn.Linear(3, 64),
+                torch.nn.ELU(),
+                torch.nn.Linear(64, 64),
+                torch.nn.ELU(),
+                torch.nn.Linear(64, 64),
+                torch.nn.ELU(),
+                torch.nn.Linear(64, 1)
+                )
+        self.Lambda=nn.Parameter(torch.Tensor([-1]))	#eigenvalue
+        self.alpha=nn.Parameter(torch.Tensor([1]))#coefficient for decay
+        self.beta=nn.Parameter(torch.Tensor([8]))#coefficient for decay
+    def forward(self,x):
+        return self.NN(x)
+        #return self.NN(d)[:,0]*torch.exp(-F.softplus(torch.abs(self.alpha*torch.norm(x,dim=1))-self.beta))
+
+net=Net()
+#x=Variable(torch.tensor([2.,2.,3.]),requires_grad=True)
+#y = net(x)
+#grad = torch.autograd.grad(y,x, create_graph=True)#,grad_outputs=torch.ones(2))
+#lap  = torch.autograd.grad(grad,x,grad_outputs=torch.ones(3))
+#print(grad,lap)
+
+
+#X1=Variable(torch.tensor([1,2,3,4.]),requires_grad=True)
+#X2=Variable(2*torch.tensor([1,2,3,4.]),requires_grad=True)
+#X3=Variable(3*torch.tensor([1,2,3,4.]),requires_grad=True)
+X1=torch.tensor([1.,2])
+X1.requires_grad=True
+X2=torch.tensor([2.,3])
+X2.requires_grad=True
+X3=torch.tensor([3.,4])
+X3.requires_grad=True
+X=torch.cat([X1,X2,X3], dim=0)
+X=X.reshape(3,2).transpose(0,1)
+#X=torch.stack([X1,X2,X3]).transpose(0,1)
+
+#n=len(X)
+#print(X)
+#print((X**2).type())
+A=net(X).flatten()
+#A=X1**2+X2**2+X3**2
+print(A)
+#G1 =torch.autograd.grad(A,X1,grad_outputs=torch.ones_like(A),create_graph=True,retain_graph=True)
+
+G1 =torch.autograd.grad(A,X1,create_graph=True,retain_graph=True,grad_outputs=torch.ones(2))
+print(G1)
+ddx=torch.autograd.grad(G1[0].flatten(),X1,retain_graph=True,grad_outputs=torch.ones(2))
+print(ddx)
+G2 =torch.autograd.grad(A,X2,create_graph=True,retain_graph=True,grad_outputs=torch.ones(2))
+print(G1)
+ddy=torch.autograd.grad(G2[0].flatten(),X2,retain_graph=True,grad_outputs=torch.ones(2))
+print(ddy)
+G3 =torch.autograd.grad(A,X3,create_graph=True,retain_graph=True,grad_outputs=torch.ones(2))
+print(G3)
+ddz=torch.autograd.grad(G3[0].flatten(),X3,retain_graph=True,grad_outputs=torch.ones(2))
+print(ddz)
+
+#ddx=torch.autograd.grad(G1,X1,grad_outputs=torch.ones(1,3))
+#G2 =torch.autograd.grad(A,X2,grad_outputs=torch.ones(n),create_graph=True,retain_graph=True)
+#ddy=torch.autograd.grad(G2,X2,grad_outputs=torch.ones(n))
+#G3 =torch.autograd.grad(A,X3,grad_outputs=torch.ones(n),create_graph=True,retain_graph=True)
+#ddz=torch.autograd.grad(G3,X3,grad_outputs=torch.ones(n))
+
+
+
+
+exit(0)
+
+#G2=grad(A,X2,create_graph=True,grad_outputs=torch.ones(n))[0]
+#G3=grad(A,X3,create_graph=True,grad_outputs=torch.ones(n))[0]
+
+ddx=grad(torch.sum(G1[0]),X1,allow_unused=True)
+#ddy=grad(G2,X2,grad_outputs=torch.ones(n), retain_graph=True)
+#ddz=grad(G3,X3,grad_outputs=torch.ones(n), retain_graph=True)
+print(ddx)
+#print(ddx,ddy,ddz)
+#print(grad(G1[0][:,1],X,grad_outputs=torch.ones(n)))
+exit(0)
+
+
+
+L=torch.zeros_like(X)
+for i in range(3):
+    print(grad(G[0][:,i],X[:,i],grad_outputs=torch.ones(n)))
+    L[:,i]=grad(G[0][:,i],X[:,i],grad_outputs=torch.ones(n))[0]
+L=torch.sum(L,dim=1)
+print(X)
+print(A)
+print(G)
+exit(0)
 
 xmax=2
 for alpha in [2,3,5,7]:
@@ -20,4 +114,3 @@ for alpha in [2,3,5,7]:
         plt.plot(d.numpy(),decay.numpy(),marker='.',ls='',label='a = '+str(alpha)+'  b = '+str(beta))
 plt.legend(loc='upper left')
 plt.show()
-
