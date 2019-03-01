fc1e69f6aaa40432ad2cc5f8c697c6ed9dbf271f
Jan Hermann <dev@jan.hermann.name>
2019-03-01 19:02:04 +0100

    clean up tools

--- src/dlqmc/utils.py
+++ src/dlqmc/utils.py
@@ -1,9 +1,10 @@
+import operator
+from functools import reduce
+
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from pyscf import gto
-
-from .Examples import Potential
 
 
 def plot_func(x, f, plot=plt.plot, **kwargs):
@@ -16,40 +17,37 @@ def get_3d_cube_mesh(bounds, npts):
     return torch.stack(grids).view(3, -1).t()
 
 
-def laplacian(x, wf):
-
-    dimension = x.shape[-1]
-    batchsize = x.shape[0]
-
-    X = []
-    for i in range(dimension):
-        X.append(x[:, i])
-        X[i].requires_grad = True
-
-    x = torch.cat(X, dim=0).reshape(dimension, batchsize).transpose(0, 1)
-    Psi = wf(x).flatten()
-
-    lap = 0
-    for i in range(dimension):
-        d = torch.autograd.grad(
-            Psi,
-            X[i],
-            create_graph=True,
-            retain_graph=True,
-            grad_outputs=torch.ones(batchsize),
-        )[0]
-        dd = torch.autograd.grad(
-            d, X[i], retain_graph=True, grad_outputs=torch.ones(batchsize)
-        )[0]
-        lap += dd
-
-    return lap, Psi
+def nuclear_energy(coords, charges):
+    coul_IJ = charges[:, None] * charges / (coords[:, None] - coords).norm(dim=-1)
+    coul_IJ[np.diag_indices(len(coords))] = 0
+    return coul_IJ.sum() / 2
 
 
-def local_energy(wf, r, coords, charges):
-    V = Potential(r, coords, charges)
+def nuclear_potential(r, coords, charges):
+    return -(charges / (r[:, None] - coords).norm(dim=-1)).sum(-1)
+
+
+def laplacian(r, wf):
+    ris = [ri.requires_grad_() for ri in r.t()]
+    psi = wf(torch.stack(ris).t()).flatten()
+    ones = torch.ones_like(psi)
+
+    def d2psi_dxi2_from_xi(ri):
+        dpsi_dxi, = torch.autograd.grad(psi, ri, grad_outputs=ones, create_graph=True)
+        d2psi_dxi2, = torch.autograd.grad(
+            dpsi_dxi, ri, grad_outputs=ones, retain_graph=True
+        )
+        return d2psi_dxi2
+
+    lap = reduce(operator.add, map(d2psi_dxi2_from_xi, ris))
+    return lap, psi
+
+
+def local_energy(r, wf, coords, charges):
+    E_nuc = nuclear_energy(coords, charges)
+    V_nuc = nuclear_potential(r, coords, charges)
     lap_psi, psi = laplacian(r, wf)
-    return -0.5 * lap_psi / psi + V
+    return -0.5 * lap_psi / psi + V_nuc + E_nuc
 
 
 def wf_from_mf(r, mf, i_mo):
@@ -57,16 +55,20 @@ def wf_from_mf(r, mf, i_mo):
     coords = torch.Tensor(mol.atom_coords())
     elems = [mol.atom_symbol(i) for i in range(mol.natm)]
     basis = mol._basis
-    i_bas = 0
-    psi = torch.zeros(r.shape[0])
-    mo_coeff = torch.Tensor(mf.mo_coeff[:, i_mo])
-    for elem, coord in zip(elems, coords):
-        x_sq = ((r - coord) ** 2).sum(dim=-1)
-        for l, *gtos in basis[elem]:
+    mo_coeffs = torch.Tensor(mf.mo_coeff[:, i_mo])
+
+    def basis_funcs():
+        for elem, coord in zip(elems, coords):
+            x_sq = ((r - coord) ** 2).sum(dim=-1)
+            for l, *gtos in basis[elem]:
+                yield l, gtos, x_sq
+
+    def psiis():
+        for (l, gtos, x_sq), mo_coeff in zip(basis_funcs(), mo_coeffs):
             assert l == 0
             g_exps, g_coeffs = torch.Tensor(gtos).t()
             g_norms = gto.gto_norm(0, g_exps) / np.sqrt(4 * np.pi)
-            ao = (g_coeffs * g_norms * torch.exp(-g_exps * x_sq[:, None])).sum(dim=-1)
-            psi += mo_coeff[i_bas] * ao
-            i_bas += 1
-    return psi
+            g_contribs = g_coeffs * g_norms * torch.exp(-g_exps * x_sq[:, None])
+            yield mo_coeff * g_contribs.sum(dim=-1)
+
+    return reduce(operator.add, psiis())
