79a96a7fca864fa072e1b51b8bfd76ce2ef2344d
Jan Hermann <dev@jan.hermann.name>
2019-03-11 17:06:08 +0100

    make things work on cuda

--- src/dlqmc/gto.py
+++ src/dlqmc/gto.py
@@ -8,23 +8,26 @@ from pyscf import gto
 
 def wf_from_mf(r, mf, i_mo):
     mol = mf.mol
-    coords = torch.Tensor(mol.atom_coords())
+    coords = r.new_tensor(mol.atom_coords())
     elems = [mol.atom_symbol(i) for i in range(mol.natm)]
     basis = mol._basis
-    mo_coeffs = torch.Tensor(mf.mo_coeff[:, i_mo])
+    mo_coeffs = r.new_tensor(mf.mo_coeff[:, i_mo])
 
     def basis_funcs():
         for elem, coord in zip(elems, coords):
-            x_sq = ((r - coord) ** 2).sum(dim=-1)
+            r_sq = ((r - coord) ** 2).sum(dim=-1)
             for l, *gtos in basis[elem]:
-                yield l, gtos, x_sq
+                yield l, gtos, r_sq
 
     def psiis():
-        for (l, gtos, x_sq), mo_coeff in zip(basis_funcs(), mo_coeffs):
+        for (l, gtos, r_sq), mo_coeff in zip(basis_funcs(), mo_coeffs):
             assert l == 0
-            g_exps, g_coeffs = torch.Tensor(gtos).t()
+            g_exps, g_coeffs = np.array(gtos).T
             g_norms = gto.gto_norm(l, g_exps) / np.sqrt(4 * np.pi)
-            g_contribs = g_coeffs * g_norms * torch.exp(-g_exps * x_sq[:, None])
+            g_exps, g_norms, g_coeffs = (
+                r.new_tensor(x) for x in (g_exps, g_norms, g_coeffs)
+            )
+            g_contribs = g_coeffs * g_norms * torch.exp(-g_exps * r_sq[:, None])
             yield mo_coeff * g_contribs.sum(dim=-1)
 
     return reduce(operator.add, psiis())
--- src/dlqmc/sampling.py
+++ src/dlqmc/sampling.py
@@ -5,9 +5,9 @@ from .physics import quantum_force
 from .utils import assign_where
 
 
-def langevin_monte_carlo(wf, n_walker, n_steps, tau, range=range):
-    samples = torch.empty(n_walker, n_steps, 3)
-    r = torch.randn(n_walker, 3)
+def langevin_monte_carlo(wf, r, n_steps, tau, range=range):
+    n_walker = r.shape[0]
+    samples = r.new_empty(n_walker, n_steps, 3)
     force, psi = quantum_force(r, wf)
     n_accepted = 0
     for i in range(n_steps):
@@ -18,8 +18,8 @@ def langevin_monte_carlo(wf, n_walker, n_steps, tau, range=range):
         ).sum(dim=-1)
         P_acc = torch.exp(log_G_ratio) * psi_new ** 2 / psi ** 2
         accepted = P_acc > torch.rand_like(P_acc)
-        n_accepted += sum(accepted.type(torch.int))
+        n_accepted += accepted.type(torch.int).sum().item()
         samples[:, i, :] = r
         assign_where((r, psi, force), (r_new, psi_new, force_new), accepted)
-    info = {'acceptance': n_accepted.item() / (n_walker * n_steps)}
+    info = {'acceptance': n_accepted / (n_walker * n_steps)}
     return samples, info
