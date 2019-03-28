0b97b10aae95eee5233a9b6bc08743b72679771f
Zeno Schaetzle <zenoone@seahorse.imp.fu-berlin.de>
2019-03-28 10:51:57 +0100

    get back on track

--- src/dlqmc/sampling.py
+++ src/dlqmc/sampling.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from tqdm import trange
 
 from .physics import quantum_force
 from .utils import assign_where
@@ -23,3 +24,131 @@ def langevin_monte_carlo(wf, r, n_steps, tau, range=range):
         assign_where((r, psi, force), (r_new, psi_new, force_new), accepted)
     info = {'acceptance': n_accepted / (n_walker * n_steps)}
     return samples, info
+    
+def dynamics(dist, pos, stepsize, steps):
+
+    pos = pos.detach().clone()
+    pos.requires_grad = True
+    vel = torch.randn(pos.shape)
+    v_te2 = (
+        vel
+        - stepsize
+        * torch.autograd.grad(
+            dist(pos),
+            pos,
+            create_graph=True,
+            retain_graph=True,
+            grad_outputs=torch.ones(pos.shape[0]),
+        )[0]
+        / 2
+    )
+    p_te = pos + stepsize * v_te2
+    for _ in range(1, steps):
+        v_te2 = (
+            v_te2
+            - stepsize
+            * torch.autograd.grad(
+                dist(p_te),
+                p_te,
+                create_graph=True,
+                retain_graph=True,
+                grad_outputs=torch.ones(pos.shape[0]),
+            )[0]
+        )
+        p_te = p_te + stepsize * v_te2
+
+    v_te = (
+        v_te2
+        - stepsize / 2
+        * torch.autograd.grad(
+            dist(p_te),
+            p_te,
+            create_graph=True,
+            retain_graph=True,
+            grad_outputs=torch.ones(pos.shape[0]),
+        )[0])
+
+    return p_te, v_te, vel
+
+
+def hmc(
+    dist,
+    stepsize,
+    dysteps,
+    n_walker,
+    steps,
+    dim,
+    startfactor=1,
+    presteps=200,
+):
+
+    acc = 0
+    samples = torch.zeros(steps, n_walker, dim)
+    walker = torch.randn(n_walker, dim) * startfactor
+    distwalker = dist(walker).detach().numpy()
+
+    for i in trange(steps + presteps):
+
+        if i >= presteps:
+            samples[i - presteps] = walker
+
+        trial, v_trial, v_0 = dynamics((lambda x: -torch.log(dist(x))), walker, stepsize, dysteps)
+        disttrial = dist(trial).detach().numpy()
+        ratio = torch.from_numpy(disttrial / distwalker) * (
+            torch.exp(
+                -0.5
+                * (torch.sum(v_trial ** 2, dim=-1) - torch.sum(v_0 ** 2, dim=-1))
+            )
+        )
+        R = torch.rand(n_walker)
+        smaller = (ratio < R).type(torch.LongTensor)
+        larger = torch.abs(smaller - 1)
+        ind = torch.nonzero(larger).flatten()
+        walker[ind] = trial[ind]
+        distwalker[ind] = disttrial[ind]
+        if i >= presteps:
+            acc += torch.sum(larger).item()
+
+    print('Acceptanceratio: ' + str(np.round(acc / (n_walker * steps) * 100, 2)) + '%')
+    return samples
+
+
+def metropolis(
+    distribution,
+    startpoint,
+    stepsize,
+    steps,
+    dim,
+    n_walker,
+    startfactor=0.2,
+    presteps=0,
+    interval=None,
+    T=0.2,
+):
+
+    samples = torch.zeros(steps, n_walker, len(startpoint))
+    ratios = np.zeros((steps, n_walker))
+    walker = torch.randn(n_walker, dim) * startfactor
+    distwalker = distribution(walker)
+    for i in trange(presteps + steps):
+        if i > (presteps - 1):
+            samples[i - presteps] = walker
+        pro = (torch.rand(walker.shape) - 0.5) * stepsize
+        trial = walker + pro
+        disttrial = distribution(trial)
+        
+        if interval is not None:
+            inint = torch.tensor(
+                all(torch.tensor(interval[0]).type(torch.FloatTensor) < trial[0])
+                and all(torch.tensor(interval[1]).type(torch.FloatTensor) > trial[0])
+            ).type(torch.FloatTensor)
+            disttrial = disttrial * inint
+
+        ratio = np.exp((disttrial.detach().numpy() - distwalker.detach().numpy()) / T)
+        ratios[i - presteps] = ratio
+        smaller_n = (ratio < np.random.uniform(0, 1, n_walker)).astype(float)
+        smaller = torch.from_numpy(smaller_n).type(torch.FloatTensor)
+        larger = torch.abs(smaller - 1)
+        walker = trial * larger[:, None] + walker * smaller[:, None]
+
+    return samples
