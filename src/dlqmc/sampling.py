4e2b140d2f87f01d596497ba949758aafab1409b
Jan Hermann <dev@jan.hermann.name>
2019-03-28 12:03:09 +0100

    cleanup

--- src/dlqmc/sampling.py
+++ src/dlqmc/sampling.py
@@ -7,25 +7,6 @@ from .physics import quantum_force
 from .utils import assign_where
 
 
-def langevin_monte_carlo(wf, r, n_steps, tau, range=range):
-    n_walker = r.shape[0]
-    samples = r.new_empty(n_walker, n_steps, 3)
-    force, psi = quantum_force(r, wf)
-    n_accepted = 0
-    for i in range(n_steps):
-        r_new = r + force * tau + torch.randn_like(r) * np.sqrt(tau)
-        force_new, psi_new = quantum_force(r_new, wf)
-        log_G_ratio = (
-            (force + force_new) * ((r - r_new) + tau / 2 * (force - force_new))
-        ).sum(dim=-1)
-        P_acc = torch.exp(log_G_ratio) * psi_new ** 2 / psi ** 2
-        accepted = P_acc > torch.rand_like(P_acc)
-        n_accepted += accepted.type(torch.int).sum().item()
-        samples[:, i] = r
-        assign_where((r, psi, force), (r_new, psi_new, force_new), accepted)
-    info = {'acceptance': n_accepted / (n_walker * n_steps)}
-    return samples, info
-    
 def dynamics(dist, pos, stepsize, steps):
 
     pos = pos.detach().clone()
@@ -60,28 +41,21 @@ def dynamics(dist, pos, stepsize, steps):
 
     v_te = (
         v_te2
-        - stepsize / 2
+        - stepsize
+        / 2
         * torch.autograd.grad(
             dist(p_te),
             p_te,
             create_graph=True,
             retain_graph=True,
             grad_outputs=torch.ones(pos.shape[0]),
-        )[0])
+        )[0]
+    )
 
     return p_te, v_te, vel
 
 
-def hmc(
-    dist,
-    stepsize,
-    dysteps,
-    n_walker,
-    steps,
-    dim,
-    startfactor=1,
-    presteps=200,
-):
+def hmc(dist, stepsize, dysteps, n_walker, steps, dim, startfactor=1, presteps=200):
 
     acc = 0
     samples = torch.zeros(steps, n_walker, dim)
@@ -93,12 +67,13 @@ def hmc(
         if i >= presteps:
             samples[i - presteps] = walker
 
-        trial, v_trial, v_0 = dynamics((lambda x: -torch.log(dist(x))), walker, stepsize, dysteps)
+        trial, v_trial, v_0 = dynamics(
+            (lambda x: -torch.log(dist(x))), walker, stepsize, dysteps
+        )
         disttrial = dist(trial).detach().numpy()
         ratio = torch.from_numpy(disttrial / distwalker) * (
             torch.exp(
-                -0.5
-                * (torch.sum(v_trial ** 2, dim=-1) - torch.sum(v_0 ** 2, dim=-1))
+                -0.5 * (torch.sum(v_trial ** 2, dim=-1) - torch.sum(v_0 ** 2, dim=-1))
             )
         )
         R = torch.rand(n_walker)
@@ -137,7 +112,7 @@ def metropolis(
         pro = (torch.rand(walker.shape) - 0.5) * stepsize
         trial = walker + pro
         disttrial = distribution(trial)
-        
+
         if interval is not None:
             inint = torch.tensor(
                 all(torch.tensor(interval[0]).type(torch.FloatTensor) < trial[0])
@@ -153,6 +128,7 @@ def metropolis(
         walker = trial * larger[:, None] + walker * smaller[:, None]
 
     return samples
+
 
 def samples_from(sampler, steps, *, n_discard=0):
     samples, infos = zip(*(step for step, i in zip(sampler, steps) if i >= n_discard))
@@ -172,4 +148,3 @@ def langevin_monte_carlo(wf, rs, *, tau):
         info = {'acceptance': accepted.type(torch.int).sum().item() / rs.shape[0]}
         yield rs.clone(), info
         assign_where((rs, psis, forces), (rs_new, psis_new, forces_new), accepted)
-
