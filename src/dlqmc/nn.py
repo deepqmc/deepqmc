4251f6d337da47fcddf2205a9955b23b464786e8
Zeno Schaetzle <zenoone@seahorse.imp.fu-berlin.de>
2019-04-02 14:24:28 +0200

    added a first version of the antisiymmetric part consisting of the product of pairwise antisymmetric functions

--- src/dlqmc/nn.py
+++ src/dlqmc/nn.py
@@ -62,6 +62,32 @@ class SSP(nn.Softplus):
     def forward(self, xs):
         return ssp(xs, self.beta, self.threshold)
 
+class NetPairwiseAntisymmetry(nn.Module):
+    def __init__(self, net_pair):
+        super().__init__()
+        self.net_pair = net_pair
+
+    def forward(self, x_i, x_j):
+        return self.net_pair(x_i,x_j) - self.net_pair(x_j,x_i)
+
+class NetOdd(nn.Module):
+    def __init__(self, net):
+        super().__init__()
+        self.net = net
+        
+    def forward(self, x):
+        return self.net(x)-(self.net(-x))
+        
+class AntisymmetricPart(nn.Module):
+    def __init__(self, net, net_pair):
+        super().__init__()
+        self.net_pair_anti = NetPairwiseAntisymmetry(net_pair)
+        self.latentdim = list(net.parameters())[0].shape[1]
+        self.net_odd = NetOdd(net)
+        
+    def forward(self, x):
+        i, j = np.triu_indices(x.shape[-2], k=1)
+        return self.net_odd(torch.prod(self.net_pair_anti(x[:,j].view(-1,3),x[:,i].view(-1,3)).view(-1,len(i),self.latentdim),dim=1))
 
 class WFNet(nn.Module):
     def __init__(
@@ -98,3 +124,41 @@ class WFNet(nn.Module):
         xs, (dists_nuc, dists_el) = self._featurize(rs)
         ys = self.deep_lin(xs).squeeze(dim=1)
         return torch.exp(ys) * self.nuc_asymp(dists_nuc)
+        
+        
+        
+class WFNetAnti(nn.Module):
+    def __init__(
+        self, geom, n_electrons, net, net_pair, ion_pot=0.5, cutoff=10.0, n_dist_feats=32, alpha=1.0
+    ):
+        super().__init__()
+        self.dist_basis = DistanceBasis(n_dist_feats)
+        self.nuc_asymp = NuclearAsymptotic(geom.charges, ion_pot, alpha=alpha)
+        self.geom = geom
+        n_atoms = len(geom.charges)
+        n_pairs = n_electrons * n_atoms + n_electrons * (n_electrons - 1) // 2
+        self.deep_lin = nn.Sequential(
+            nn.Linear(n_pairs * n_dist_feats, 64),
+            SSP(),
+            nn.Linear(64, 64),
+            SSP(),
+            nn.Linear(64, 1),
+        )
+        self.antisym = AntisymmetricPart(net,net_pair)
+        self._pdist = PairwiseDistance3D()
+        self._psdist = PairwiseSelfDistance3D()
+
+    def _featurize(self, rs):
+        dists_nuc = self._pdist(rs, self.geom.coords[None, ...])
+        dists_el = self._psdist(rs)
+        dists = torch.cat([dists_nuc.flatten(start_dim=1), dists_el], dim=1)
+        xs = self.dist_basis(dists)#.flatten(start_dim=1)
+        print("yo",xs.shape)
+        print(rs.shape)
+        return xs.flatten(start_dim=1), (dists_nuc, dists_el)
+
+    def forward(self, rs):
+        xs, (dists_nuc, dists_el) = self._featurize(rs)
+        ys = self.deep_lin(xs).squeeze(dim=1)
+        return torch.exp(ys) * self.nuc_asymp(dists_nuc) * self.antisym(rs)
+
