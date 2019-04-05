77df8feb2f0e85459b456e7abe67455153081923
Jan Hermann <dev@jan.hermann.name>
2019-04-05 17:10:46 +0200

    black style

--- src/dlqmc/sampling.py
+++ src/dlqmc/sampling.py
@@ -1,54 +1,58 @@
 import numpy as np
 import pandas as pd
 import torch
-from tqdm import trange
 
+from . import torchext
 from .physics import quantum_force
 from .utils import assign_where
 
-    
+
 def dynamics(wf, pos, stepsize, steps):
+    pos = pos.detach().clone()
+    pos.requires_grad = True
+    vel = torch.randn(pos.shape)
+    forces, psis = quantum_force(pos, wf)
+    v_te2 = vel + stepsize * forces
+    p_te = pos + stepsize * v_te2
+    for _ in range(1, steps):
+        forces, psis = quantum_force(p_te, wf)
+        v_te2 = v_te2 + stepsize * 2 * forces
+        p_te = p_te + stepsize * v_te2
+    forces, psis = quantum_force(p_te, wf)
+    v_te = v_te2 + stepsize * forces
+    return p_te, v_te, vel
 
-	pos = pos.detach().clone()
-	pos.requires_grad = True
-
-	vel = torch.randn(pos.shape)
-	forces, psis = quantum_force(pos, wf)
-
-	v_te2 = (vel + stepsize * forces)
-	p_te = pos + stepsize * v_te2
-	
-	for _ in range(1, steps):
-		forces, psis = quantum_force(p_te, wf)
-		v_te2 = (v_te2 + stepsize *2*forces)
-		p_te = p_te + stepsize * v_te2
-
-	forces, psis = quantum_force(p_te, wf)
-	v_te = (v_te2 + stepsize * forces)
-
-	return p_te, v_te, vel
-	
 
 def hmc(wf, rs, *, dysteps, stepsize):
+    while True:
+        rs_new, v, v_0 = dynamics(wf, rs, stepsize, dysteps)
+        Ps_acc = (
+            wf(rs_new) ** 2
+            / wf(rs) ** 2
+            * (
+                torch.exp(
+                    -0.5
+                    * (
+                        torch.sum(v ** 2, dim=[-1, -2])
+                        - torch.sum(v_0 ** 2, dim=[-1, -2])
+                    )
+                )
+            )
+        )
+        accepted = Ps_acc > torch.rand_like(Ps_acc)
+        info = {'acceptance': accepted.type(torch.int).sum().item() / rs.shape[0]}
+        yield rs.clone(), info
+        assign_where((rs,), (rs_new,), accepted)
 
-	while True:
-		rs_new, v, v_0 = dynamics(wf, rs, stepsize, dysteps)
-		Ps_acc = wf(rs_new)**2/ wf(rs)**2 * (torch.exp(-0.5 * \
-		(torch.sum(v ** 2, dim=[-1,-2]) - torch.sum(v_0 ** 2, dim=[-1,-2]))))
-		accepted = Ps_acc > torch.rand_like(Ps_acc)
-		info = {'acceptance': accepted.type(torch.int).sum().item() / rs.shape[0]}
-		yield rs.clone(), info
-		assign_where((rs,), (rs_new,), accepted)
 
 def metropolis(wf, rs, *, stepsize):
-
-	while True:
-		rs_new = torch.randn_like(rs)* stepsize
-		Ps_acc = wf(rs_new)**2/ wf(rs)**2 
-		accepted = Ps_acc > torch.rand_like(Ps_acc)
-		info = {'acceptance': accepted.type(torch.int).sum().item() / rs.shape[0]}
-		yield rs.clone(), info
-		assign_where((rs,), (rs_new,), accepted)
+    while True:
+        rs_new = torch.randn_like(rs) * stepsize
+        Ps_acc = wf(rs_new) ** 2 / wf(rs) ** 2
+        accepted = Ps_acc > torch.rand_like(Ps_acc)
+        info = {'acceptance': accepted.type(torch.int).sum().item() / rs.shape[0]}
+        yield rs.clone(), info
+        assign_where((rs,), (rs_new,), accepted)
 
 
 def samples_from(sampler, steps, *, n_discard=0):
@@ -57,18 +61,15 @@ def samples_from(sampler, steps, *, n_discard=0):
 
 
 def langevin_monte_carlo(wf, rs, *, tau):
-	forces, psis = quantum_force(rs, wf)
-	while True:
-		rs_new = rs + forces * tau + torch.randn_like(rs) * np.sqrt(tau)
-		forces_new, psis_new = quantum_force(rs_new, wf)
-		log_G_ratios = (
-			(forces + forces_new) * ((rs - rs_new) + tau / 2 * (forces - forces_new))
-		).sum(dim=(-1, -2))
-		Ps_acc = torch.exp(log_G_ratios) * psis_new ** 2 / psis ** 2
-		accepted = Ps_acc > torch.rand_like(Ps_acc)
-		info = {'acceptance': accepted.type(torch.int).sum().item() / rs.shape[0]}
-		yield rs.clone(), info
-		assign_where((rs, psis, forces), (rs_new, psis_new, forces_new), accepted)
-        
-        
-        
+    forces, psis = quantum_force(rs, wf)
+    while True:
+        rs_new = rs + forces * tau + torch.randn_like(rs) * np.sqrt(tau)
+        forces_new, psis_new = quantum_force(rs_new, wf)
+        log_G_ratios = (
+            (forces + forces_new) * ((rs - rs_new) + tau / 2 * (forces - forces_new))
+        ).sum(dim=(-1, -2))
+        Ps_acc = torch.exp(log_G_ratios) * psis_new ** 2 / psis ** 2
+        accepted = Ps_acc > torch.rand_like(Ps_acc)
+        info = {'acceptance': accepted.type(torch.int).sum().item() / rs.shape[0]}
+        yield rs.clone(), info
+        assign_where((rs, psis, forces), (rs_new, psis_new, forces_new), accepted)
