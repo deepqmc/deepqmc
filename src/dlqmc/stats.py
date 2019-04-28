73192d20dc9bb054d1e74d5e94ad6f32d5fc938e
Jan Hermann <dev@jan.hermann.name>
2019-04-28 17:13:51 +0200

    add weights to kde

--- src/dlqmc/stats.py
+++ src/dlqmc/stats.py
@@ -3,11 +3,11 @@ import torch
 
 
 class GaussianKDEstimator:
-    def __init__(self, xs, max_memory=1.0, *, bw):
+    def __init__(self, xs, max_memory=1.0, weights=None, *, bw):
         assert len(xs.shape) == 2
         self._xs = xs
+        self._weights = weights if weights is not None else None
         self._width = np.sqrt(2) * bw
-        self._n_pts = len(xs)
         self._bs = int(2 ** 30 * max_memory) // (xs.nelement() * xs.element_size())
 
     def __call__(self, xs):
@@ -15,5 +15,8 @@ class GaussianKDEstimator:
         if len(xs) > self._bs:
             return torch.cat([self(xs_batch) for xs_batch in xs.split(self._bs)])
         kernel = ((xs[:, None] - self._xs) ** 2).sum(dim=-1) / self._width ** 2
-        norm = 1 / (self._n_pts * np.sqrt(np.pi) * self._width)
-        return norm * torch.exp(-kernel).sum(dim=-1)
+        norm = 1 / (len(self._xs) * (np.sqrt(np.pi) * self._width) ** xs.shape[1])
+        basis = torch.exp(-kernel)
+        if self._weights is not None:
+            basis = self._weights * basis
+        return norm * basis.sum(dim=-1)
