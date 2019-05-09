780338fee1c63b06a7df66b29e936a816d09a7e9
Jan Hermann <dev@jan.hermann.name>
2019-05-09 17:02:19 +0200

    add analysis.pair_correlations_from_samples()
    this function calculates GKD estimates of pair correlation functions
    for all spin combinations

--- src/dlqmc/analysis.py
+++ src/dlqmc/analysis.py
@@ -1,6 +1,10 @@
 import numpy as np
 import torch
 
+from .nn.base import pairwise_distance, pairwise_self_distance
+from .stats import GaussianKDEstimator
+from .utils import shuffle_tensor
+
 
 def blocking(xs, max_B=None):
     N = xs.shape[1]
@@ -30,3 +34,17 @@ def autocorr_coeff(ks, xs):
         x_autocov = ((xs[:, :end] - x_mean) * (xs[:, k:] - x_mean)).mean()
         Cs.append(x_autocov / x_var)
     return torch.tensor(Cs)
+
+
+def pair_correlations_from_samples(rs, n_up, bw=0.1):
+    R_uu = pairwise_self_distance(rs[:, :n_up]).flatten()
+    R_dd = pairwise_self_distance(rs[:, n_up:]).flatten()
+    R_ud = pairwise_distance(rs[:, :n_up], rs[:, n_up:]).flatten()
+    rs_decorr = shuffle_tensor(rs.view(-1, 3)).view(-1, 2, 3)
+    R_decorr = pairwise_self_distance(rs_decorr)[:, 0]
+    return {
+        'uu': GaussianKDEstimator(R_uu[:, None], bw=bw),
+        'dd': GaussianKDEstimator(R_dd[:, None], bw=bw),
+        'ud': GaussianKDEstimator(R_ud[:, None], bw=bw),
+        'decorr': GaussianKDEstimator(R_decorr[:, None], bw=bw),
+    }
