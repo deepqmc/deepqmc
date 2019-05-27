5c8e8ac7492522563ce2a317bbafc33529b81e3b
Jan Hermann <dev@jan.hermann.name>
2019-05-27 20:57:59 +0200

    remove use of bias in last layer in wfnet

--- src/dlqmc/nn/wfnet.py
+++ src/dlqmc/nn/wfnet.py
@@ -35,7 +35,7 @@ class WFNet(BaseWFNet):
         self.asymp_elec = ElectronicAsymptotic(cusp=cusp) if cusp is not None else None
         n_pairs = n_electrons * len(geom) + n_electrons * (n_electrons - 1) // 2
         self.orbital = get_log_dnn(
-            n_pairs * basis_dim, 1, SSP, n_layers=n_orbital_layers
+            n_pairs * basis_dim, 1, SSP, n_layers=n_orbital_layers, last_bias=False
         )
 
     def tracked_parameters(self):
