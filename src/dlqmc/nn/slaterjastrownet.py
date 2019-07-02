edf1fcd7afe1df2b9c8e38ca824aae13f1f273f3
Jan Hermann <dev@jan.hermann.name>
2019-07-02 17:23:08 +0200

    refactor SlaterJastrowNet

--- src/dlqmc/nn/slaterjastrownet.py
+++ src/dlqmc/nn/slaterjastrownet.py
@@ -1,5 +1,5 @@
 import torch
-import numpy as np
+from torch import nn
 
 from ..geom import Geometry
 from ..utils import NULL_DEBUG, triu_flat
@@ -13,10 +13,6 @@ from .base import (
 )
 from .gto import GTOBasis
 from .molorb import MolecularOrbital
-
-from dlqmc.nn.molorb import MolecularOrbital
-
-from dlqmc.nn.molorb import MolecularOrbital
 
 
 class SlaterJastrowNet(BaseWFNet):
@@ -33,39 +29,29 @@ class SlaterJastrowNet(BaseWFNet):
         cusp_correction=False,
         cusp_electrons=False,
         rc_scaling=1.0,
-        activeorb = None,
-        activeel = None,
-        det_list = None,
-        
+        configurations=None,
     ):
         super().__init__()
-        self.n_up, self.n_down = n_up, n_down
-        
-        if activeorb is None:
-            self.activeorb = max(n_up,n_down)
-        else:
-            self.activeorb = activeorb
-        
-        if activeel is None:
-            self.activeel = n_up+n_down
-        else:
-            self.activeel = activeel
-            
-        if det_list is None:
-            self.det_list = [(1.,np.arange(n_up,dtype=float),np.arange(n_down,dtype=float))]
-        else:
-            self.det_list = det_list
-        
+        self.n_up = n_up
         self.register_geom(geom)
         self.dist_basis = (
             DistanceBasis(dist_basis_dim, cutoff=dist_basis_cutoff, envelope='nocusp')
             if mo_factory or jastrow_factory
             else None
         )
+        if configurations is not None:
+            assert configurations.shape[1] == n_up + n_down
+            n_orbitals = configurations.max() + 1
+            self.confs = configurations
+            self.conf_coeff = nn.Linear(len(configurations), 1, bias=False)
+        else:
+            n_orbitals = max(n_up, n_down)
+            self.confs = torch.cat([torch.arange(n_up), torch.arange(n_down)])
+            self.conf_coeff = nn.Identity()
         self.mo = MolecularOrbital(
             geom,
             basis,
-            self.activeorb,
+            n_orbitals,
             net_factory=mo_factory,
             edge_dim=dist_basis_dim,
             cusp_correction=cusp_correction,
@@ -81,37 +67,46 @@ class SlaterJastrowNet(BaseWFNet):
             if jastrow_factory
             else None
         )
-        
 
-    def init_from_pyscf(self, pyscf_input, **kwargs):
-        self.mo.init_from_pyscf(pyscf_input, **kwargs)
-
-        
     @classmethod
-    def from_pyscf(cls, pyscf_input, cusp_correction=False, mo_cutoff=1e-3, **kwargs):
-        
-        n_up,n_down = pyscf_input.mol.nelec
-
+    def from_pyscf(
+        cls,
+        mf,
+        init_weights=True,
+        freeze_mos=False,
+        freeze_confs=False,
+        conf_cutoff=1e-3,
+        **kwargs,
+    ):
+        n_up, n_down = mf.mol.nelec
         try:
-            activeorb = pyscf_input.ncas
-            activeel  = pyscf_input.nelecas
-            det_list  = pyscf_input.fcisolver.large_ci(pyscf_input.ci, pyscf_input.ncas, pyscf_input.nelecas, tol=mo_cutoff, return_strs=False)
-
-        except:
-            activeorb = max(n_up,n_down)
-            activeel  = pyscf_input.mol.nelectron
-            det_list  = [(1.,np.arange(n_up,dtype=float),np.arange(n_down,dtype=float))]
-            
-        geom = Geometry(pyscf_input.mol.atom_coords().astype('float32'), pyscf_input.mol.atom_charges())
-        basis = GTOBasis.from_pyscf(pyscf_input.mol)
-        wf = cls(geom, n_up, n_down, basis,activeorb=activeorb,activeel=activeel,det_list=det_list, cusp_correction=cusp_correction)
-        wf.init_from_pyscf(pyscf_input)
+            conf_coeff, *confs = zip(
+                *mf.fcisolver.large_ci(
+                    mf.ci, mf.ncas, mf.nelecas, tol=conf_cutoff, return_strs=False
+                )
+            )
+            conf_coeff = torch.tensor(conf_coeff)
+            confs = (
+                torch.tensor(confs, dtype=torch.long)
+                .permute(1, 0, 2)
+                .flatten(start_dim=1)
+            )
+        except AttributeError:
+            confs = None
+        geom = Geometry(mf.mol.atom_coords().astype('float32'), mf.mol.atom_charges())
+        basis = GTOBasis.from_pyscf(mf.mol)
+        wf = cls(geom, n_up, n_down, basis, configurations=confs, **kwargs)
+        if init_weights:
+            wf.mo.init_from_pyscf(mf, freeze_mos=freeze_mos)
+            if confs is not None:
+                wf.conf_coeff.weight.detach().copy_(conf_coeff)
+                if freeze_mos:
+                    wf.conf_coeff.weight.requires_grad_(False)
         return wf
-    
 
     def forward(self, rs, debug=NULL_DEBUG):
         batch_dim, n_elec = rs.shape[:2]
-        assert n_elec == self.n_up + self.n_down
+        assert n_elec == self.confs.shape[1]
         n_atoms = len(self.coords)
         diffs_nuc = pairwise_diffs(
             torch.cat([self.coords, rs.flatten(end_dim=1)]), self.coords
@@ -121,18 +116,16 @@ class SlaterJastrowNet(BaseWFNet):
             if self.jastrow or self.mo.net
             else None
         )
-        xs = debug['mos'] = self.mo(diffs_nuc, edges_nuc, debug=debug).view(batch_dim,n_elec, self.activeorb)
-        
-        re_list = list(zip(*self.det_list))
-        coeff = torch.tensor(re_list[0]).cuda()
-        up_index = np.array(re_list[1])
-        down_index = np.array(re_list[2])
-        
-        det_up = eval_slater(xs[:, : self.n_up, up_index].permute(0,2,1,3).flatten(end_dim=1)).view(batch_dim,-1)
-        det_down = eval_slater(xs[:, self.n_up:, down_index].permute(0,2,1,3).flatten(end_dim=1)).view(batch_dim,-1)
-
-        psi = torch.sum(coeff * det_up * det_down,dim=-1)
-        
+        xs = debug['mos'] = self.mo(diffs_nuc, edges_nuc, debug=debug)
+        xs = debug['slaters'] = xs.view(batch_dim, n_elec, -1)
+        conf_up, conf_down = self.confs[:, : self.n_up], self.confs[:, self.n_up :]
+        det_up = debug['det_up'] = eval_slater(
+            xs[:, : self.n_up, conf_up].permute(0, 2, 1, 3).flatten(end_dim=1)
+        ).view(batch_dim, -1)
+        det_down = debug['det_down'] = eval_slater(
+            xs[:, self.n_up :, conf_down].permute(0, 2, 1, 3).flatten(end_dim=1)
+        ).view(batch_dim, -1)
+        psi = self.conf_coeff(det_up * det_down).squeeze(dim=-1)
         if self.cusp_same or self.jastrow:
             dists_elec = pairwise_distance(rs, rs)
         if self.cusp_same:
