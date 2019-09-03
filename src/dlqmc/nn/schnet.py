278f7397f3893c859c85f1be233b1713b9a17979
Jan Hermann <dev@jan.hermann.name>
2019-09-03 17:49:25 +0200

    implement version 2 of electronic schnet
    equations describing the architecture are in the paper

--- src/dlqmc/nn/schnet.py
+++ src/dlqmc/nn/schnet.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import numpy as np
 import torch
 from torch import nn
@@ -40,6 +42,7 @@ class ElectronicSchnet(nn.Module):
             kernel_dim, embedding_dim, basis_dim
         )
         super().__init__()
+        self.n_up = n_up
         self.n_interactions = n_interactions
         self.return_interactions = return_interactions
         self.Y = nn.Parameter(torch.randn(n_nuclei, kernel_dim))
@@ -52,29 +55,45 @@ class ElectronicSchnet(nn.Module):
             )
         )
         w, h, g = {}, {}, {}
-        for i in range(n_interactions):
-            w[f'{i}'] = w_factory()
-            g[f'{i}'] = g_factory()
-            h[f'{i}'] = h_factory()
+        for n in range(n_interactions):
+            for label in [True, False, 'n']:
+                w[f'{n},{label}'] = w_factory()
+                g[f'{n},{label}'] = g_factory()
+            h[f'{n}'] = h_factory()
         self.w, self.h, self.g = map(nn.ModuleDict, [w, h, g])
 
     def forward(self, edges, debug=NULL_DEBUG):
         edges_elec, edges_nuc = edges
-        *batch_dims, n_elec, n_atoms, basis_dim = edges_nuc.shape
-        assert edges_elec.shape == (*batch_dims, n_elec, n_elec, basis_dim)
-        i, j = np.mask_indices(n_elec, nondiag)  # nondiagonal elements
-        edges_elec = edges_elec[..., i, j, :]
-        x = debug[0] = self.X.clone().expand(*batch_dims, -1, -1)
+        *batch_dims, n_elec = edges_nuc.shape[:-2]
+        assert edges_elec.shape[:-1] == (*batch_dims, n_elec, n_elec)
         interactions = [] if self.return_interactions else None
-        for i in range(self.n_interactions):
-            z_elec = (
-                (self.w[f'{i}'](edges_elec) * self.h[f'{i}'](x)[:, j])
-                .view(*batch_dims, n_elec, n_elec - 1, -1)
+        ij = np.vstack(np.mask_indices(n_elec, nondiag)).T  # nondiagonal elements
+        spin_mask = ij < self.n_up
+        ijs_spin = [
+            ij[spin_mask.all(axis=1)].T,
+            ij[np.diff(spin_mask.astype(int))[:, 0] == -1].T,
+            ij[np.diff(spin_mask.astype(int))[:, 0] == 1].T,
+            ij[~spin_mask.any(axis=1)].T,
+        ]  # indexes for up-up, up-down, down-up, down-down
+        n_el = {'u': self.n_up, 'd': n_elec - self.n_up}
+        x = debug[0] = self.X.clone().expand(*batch_dims, -1, -1)
+        for n in range(self.n_interactions):
+            h = self.h[f'{n}'](x)
+            z_elec_uu, z_elec_ud, z_elec_du, z_elec_dd = (
+                (self.w[f'{n},{si == sj}'](edges_elec[..., i, j, :]) * h[..., j, :])
+                .view(*batch_dims, n_el[si], -1, h.shape[-1])
                 .sum(dim=-2)
+                for (si, sj), (i, j) in zip(product('ud', repeat=2), ijs_spin)
             )
-            z_nuc = (self.w[f'{i}'](edges_nuc) * self.Y[..., None, :, :]).sum(dim=-2)
-            z = self.g[f'{i}'](z_elec + z_nuc)
+            z_elec_same = torch.cat([z_elec_uu, z_elec_dd], dim=-2)
+            z_elec_anti = torch.cat([z_elec_ud, z_elec_du], dim=-2)
+            z_nuc = (self.w[f'{n},n'](edges_nuc) * self.Y[..., None, :, :]).sum(dim=-2)
+            z = (
+                self.g[f'{n},True'](z_elec_same)
+                + self.g[f'{n},False'](z_elec_anti)
+                + self.g[f'{n},n'](z_nuc)
+            )
             if interactions is not None:
                 interactions.append(z)
-            x = debug[i + 1] = x + z
+            x = debug[n + 1] = x + z
         return x if interactions is None else interactions
