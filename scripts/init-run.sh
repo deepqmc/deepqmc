1c61791c479a3e35429522f6f9c0feffa1f8a77e
Jan Hermann <dev@jan.hermann.name>
2019-12-05 12:47:30 +0100

    strip down to bare package

--- scripts/init-run.sh
+++
@@ -1,4 +0,0 @@
-echo "python = $(which python)"
-[[ -d venv ]] || { python -m venv venv --system-site-packages && venv/bin/pip install dlqmc.tar.gz[train]; }
-source venv/bin/activate
-echo "python = $(which python)"
