9270510c7c5541c10ea8d3a21199555e6b29a73c
Jan Hermann <dev@jan.hermann.name>
2020-01-23 23:17:28 +0100

    refactor optimal batch estimation

--- src/deepqmc/utils.py
+++ src/deepqmc/utils.py
@@ -162,35 +162,35 @@ class NestedDict(dict):
                 super().__setitem__(key, val)
 
 
+def unused_cuda_memory():
+    import subprocess
+
+    mem_total = torch.cuda.get_device_properties(0).total_memory / 1e6
+    out = subprocess.run(['nvidia-smi', '-q'], capture_output=True).stdout.decode()
+    mem_used = sum(int(l.split()[4]) for l in out.split('\n') if 'Used GPU Memory' in l)
+    mem_used *= 1024 ** 2 / 1e6
+    return mem_total - mem_used
+
+
 def estimate_optimal_batch_size_cuda(
-    func, test_batch_sizes, mem_margin=0.9, max_memory=None,
+    test_func, test_batch_sizes, mem_margin=0.9, max_memory=None,
 ):
     assert len(test_batch_sizes) >= 4
     test_batch_sizes = torch.as_tensor(test_batch_sizes).float()
     mem = []
     for size in test_batch_sizes.int():
         torch.cuda.reset_max_memory_allocated()
-        func(size.item())
+        test_func(size.item())
         mem.append(torch.cuda.max_memory_allocated() / 1e6)
     mem = torch.tensor(mem)
     delta = (mem[1:] - mem[:-1]) / (test_batch_sizes[1:] - test_batch_sizes[:-1])
-    delta = delta[1:]  # throw away first try
+    delta = delta[1:]  # first try may be off due to caching
     assert (delta > 0).all()
     memory_per_batch = delta.mean() / mem_margin
-    if torch.sqrt(delta.var()) / memory_per_batch > 0.3:
+    if delta.std() / memory_per_batch > 0.3:
         raise DeepQMCError(
             'Inconsistent estimation of GPU memory per batch. '
             'Try specifying large test_batch_sizes.'
         )
-    if max_memory is None:
-        import subprocess
-
-        memory_total = torch.cuda.get_device_properties(0).total_memory * 9.5367e-7
-        sp = subprocess.Popen(
-            ['nvidia-smi', '-q'], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        memory_in_use = int(
-            str(sp.communicate()).split('Used GPU Memory         : ')[1].split('MiB')[0]
-        )
-        max_memory = memory_total - memory_in_use
+    max_memory = max_memory or unused_cuda_memory()
     return int(max_memory / memory_per_batch)
