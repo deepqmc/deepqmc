98a44e50533dfc634c2f52669826e7c910eedfde
Zeno Schätzle <zenoone@physik.fu-berlin.de>
2020-03-25 10:41:56 +0100

    add heuristic to rewind the training if energetically unfavorable step is done

--- src/deepqmc/train.py
+++ src/deepqmc/train.py
@@ -32,6 +32,8 @@ def train(  # noqa: C901
     equilibrate=True,
     fit_kwargs=None,
     sampler_kwargs=None,
+    reset_memory=False,
+    reset_tolerance=0.005,
 ):
     r"""Train a wave function model.
 
@@ -104,6 +106,9 @@ def train(  # noqa: C901
     steps = trange(
         init_step, n_steps, initial=init_step, total=n_steps, desc='training'
     )
+    restart_from = False
+    if reset_memory:
+        assert workdir
     try:
         if equilibrate:
             with tqdm(count(), desc='equilibrating') as eq_steps:
@@ -138,8 +143,52 @@ def train(  # noqa: C901
                     if scheduler:
                         state['scheduler'] = scheduler.state_dict()
                     torch.save(state, chkpts_dir / f'state-{step:05d}.pt')
+            if reset_memory and step > reset_memory:
+                energy_memory = torch.from_numpy(
+                    h5file.root.fit.col('E_loc')[-reset_memory:].mean(axis=-1)
+                )
+                mean_old, mean_new = map(
+                    lambda x: x.sort()[0][
+                        reset_memory // 10 : -reset_memory // 10 - 1
+                    ].mean(),
+                    (
+                        energy_memory[: reset_memory // 2],
+                        energy_memory[reset_memory // 2 :],
+                    ),
+                )
+                if (mean_new - (1 - reset_tolerance) * mean_old) / (
+                    mean_new + mean_old
+                ) < 0:
+                    restart_from = torch.load(
+                        chkpts_dir
+                        / f'state-{int((step-reset_memory/2)//save_every*save_every-1):05d}.pt'
+                    )
+                    print(
+                        f'restore state at step {int((step-reset_memory/2)//save_every*save_every-1)}'
+                    )
+                    break
     finally:
         steps.close()
         if workdir:
             writer.close()
             h5file.close()
+        if restart_from:
+            wf.load_state_dict(restart_from['wf']),
+            train(
+                wf,
+                workdir=workdir,
+                save_every=save_every,
+                state=restart_from,
+                n_steps=n_steps,
+                batch_size=batch_size,
+                epoch_size=epoch_size,
+                optimizer=optimizer,
+                learning_rate=learning_rate,
+                lr_scheduler=lr_scheduler,
+                decay_rate=decay_rate,
+                equilibrate=True,
+                fit_kwargs=fit_kwargs,
+                sampler_kwargs=sampler_kwargs,
+                reset_memory=reset_memory,
+                reset_tolerance=reset_tolerance,
+            )
