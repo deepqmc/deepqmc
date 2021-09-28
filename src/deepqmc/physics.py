2820812e29c5c7c54776327423fce741f2222b57
Zeno Schätzle <zenoone@physik.fu-berlin.de>
2021-09-28 19:26:14 +0200

    allow nan values to propagate through the code

--- src/deepqmc/physics.py
+++ src/deepqmc/physics.py
@@ -57,8 +57,6 @@ def quantum_force(rs, wf):
     forces, psis = grad(rs, wf)
     if torch.isnan(psis[0]).any():
         raise NanError(rs)
-    if torch.isnan(forces).any():
-        raise NanError(rs)
     return forces, psis
 
 
@@ -112,10 +110,6 @@ def local_energy(
         rs, wf, create_graph=create_graph, keep_graph=keep_graph, return_grad=True
     )
     if torch.isnan(log_psis).any():
-        raise NanError(rs)
-    if torch.isnan(quantum_force).any():
-        raise NanError(rs)
-    if torch.isnan(lap_log_psis).any():
         raise NanError(rs)
     Es_loc = (
         -0.5 * (lap_log_psis + (quantum_force ** 2).sum(dim=(-2, -1)))
