f1b57ea095dbbde26ad2d32796cc3fcf4fbd15cd
Zeno Schätzle <zenoone@physik.fu-berlin.de>
2022-02-18 18:20:09 +0100

    use parallel functions based on global variable

--- src/deepqmc/batch_operations.py
+++ src/deepqmc/batch_operations.py
@@ -1,63 +1,64 @@
 import torch
 import torch.distributed as dist
 
+PARALLEL = False
 
 __all__ = ()
 
 
-def batch_len(t, parallel=False):
+def batch_len(t):
     len_t = len(t)
-    if parallel:
+    if PARALLEL:
         len_t = torch.as_tensor(len_t, dtype=t.dtype, device=t.device)
         dist.all_reduce(len_t, dist.ReduceOp.SUM)
     return len_t
 
 
-def batch_sum(t, parallel=False):
+def batch_sum(t):
     t_sum = t.sum()
-    if parallel:
+    if PARALLEL:
         dist.all_reduce(t_sum, dist.ReduceOp.SUM)
     return t_sum
 
 
-def batch_mean(t, parallel=False):
-    return batch_sum(t, parallel) / batch_len(t, parallel)
+def batch_mean(t):
+    return batch_sum(t) / batch_len(t)
 
 
-def batch_median(t, parallel=False):
-    t = batch_gather_and_concat(t, parallel)  # batch median needs all elements
+def batch_median(t):
+    t = batch_gather_and_concat(t)  # batch median needs all elements
     return t.median()
 
 
-def batch_max(t, parallel=False):
+def batch_max(t):
     t_max = t.max()
-    if parallel:
+    if PARALLEL:
         dist.all_reduce(t_max, dist.ReduceOp.MAX)
     return t_max
 
 
-def batch_min(t, parallel=False):
+def batch_min(t):
     t_min = t.min()
-    if parallel:
+    if PARALLEL:
         dist.all_reduce(t_min, dist.ReduceOp.MIN)
     return t_min
 
 
-def batch_exp_normalize_mean(t, parallel=False):
-    t_max = batch_max(t, parallel)
+def batch_exp_normalize_mean(t):
+    t_max = batch_max(t)
     t_shifted = t - t_max
-    return t_shifted.exp() / batch_mean(t_shifted.exp(), parallel)
+    return t_shifted.exp() / batch_mean(t_shifted.exp())
 
 
-def batch_weighted_mean_var(t, log_ws= None, parallel=False):
+def batch_weighted_mean_var(t, log_ws=None):
     log_ws = torch.zeros_like(t) if log_ws is None else log_ws
-    ws = batch_exp_normalize_mean(log_ws, parallel)
-    mean = batch_mean(ws * t, parallel)
-    return mean, batch_mean((ws * (t - mean) ** 2), parallel)
+    ws = batch_exp_normalize_mean(log_ws)
+    mean = batch_mean(ws * t)
+    return mean, batch_mean((ws * (t - mean) ** 2))
 
 
-def batch_gather_and_concat(t, parallel=False):
-    if parallel:
+def batch_gather_and_concat(t):
+    if PARALLEL:
         tensor_list = [torch.zeros_like(t) for _ in range(dist.get_world_size())]
         dist.all_gather(tensor_list=tensor_list, tensor=t)
         t = torch.concat(tensor_list)
