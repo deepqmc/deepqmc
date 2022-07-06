a18f7c1a80430816045fcbccdbf74304aba3cf92
Jan Hermann <dev@jan.hermann.name>
2022-07-06 17:09:31 +0200

    cleanup

--- src/deepqmc/jax/__init__.py
+++ src/deepqmc/jax/__init__.py
@@ -12,6 +12,7 @@ __all__ = ()
 
 Psi = namedtuple('Psi', 'sign log')
 TrainState = namedtuple('TrainState', 'params opt sampler')
+EWMState = namedtuple('EWMState', 'params step mean var sqerr', defaults=4 * [None])
 
 
 def laplacian(f):
@@ -196,18 +197,17 @@ def fit_wf(
 @jax.jit
 def ewm(x=None, state=None, max_alpha=0.999, decay_alpha=10):
     if x is None:
-        return {'params': {'max_alpha': max_alpha, 'decay_alpha': decay_alpha}}
-    if 'mean' not in state:
-        return {**state, 'mean': x, 'var': 0, 'sqerr': 0, 'step': 0}
-    p = state['params']
-    a = jnp.minimum(p['max_alpha'], 1 - 1 / (2 + state['step'] / p['decay_alpha']))
-    return {
-        **state,
-        'mean': (1 - a) * x + a * state['mean'],
-        'var': (1 - a) * (x - state['mean']) ** 2 + a * state['var'],
-        'sqerr': (1 - a) ** 2 * state['var'] + a**2 * state['sqerr'],
-        'step': state['step'] + 1,
-    }
+        return EWMState({'max_alpha': max_alpha, 'decay_alpha': decay_alpha})
+    if state.mean is None:
+        return state._replace(step=0, mean=x, var=0, sqerr=0)
+    p = state.params
+    a = jnp.minimum(p['max_alpha'], 1 - 1 / (2 + state.step / p['decay_alpha']))
+    return state._replace(
+        step=state.step + 1,
+        mean=(1 - a) * x + a * state.mean,
+        var=(1 - a) * (x - state.mean) ** 2 + a * state.var,
+        sqerr=(1 - a) ** 2 * state.var + a**2 * state.sqerr,
+    )
 
 
 if __name__ == '__main__':
