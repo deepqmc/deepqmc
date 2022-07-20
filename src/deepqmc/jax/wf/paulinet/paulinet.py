34db22fd911464fffb31498e2a7a531b2f5b9955
Zeno Schätzle <zenoone@physik.fu-berlin.de>
2022-07-20 17:43:45 +0200

    make PauliNet work w and wo batch dimension

--- src/deepqmc/jax/wf/paulinet/paulinet.py
+++ src/deepqmc/jax/wf/paulinet/paulinet.py
@@ -15,7 +15,7 @@ def eval_log_slater(xs):
 
 
 class PauliNet(WaveFunction):
-    def __init__(self, mol, basis, n_configurations=1, n_orbitals=None):
+    def __init__(self, mol, basis, confs=None, n_orbitals=None):
         super().__init__(mol)
         n_up, n_down = self.n_up, self.n_down
         n_orbitals = n_orbitals or max(n_up, n_down)
@@ -23,49 +23,36 @@ class PauliNet(WaveFunction):
         self.mo_coeff = hk.Linear(
             n_orbitals, with_bias=False, w_init=None, b_init=None, name='mo_coeff'
         )
+        confs = [list(range(n_up)) + list(range(n_down))] if confs is None else confs
+        self.confs = jnp.array(confs)
         self.conf_coeff = hk.Linear(
-            n_configurations,
+            len(confs),
             with_bias=False,
             w_init=None,
             b_init=None,
             name='conf_coeff',
         )
-        self.confs = jnp.array(
-            [list(range(n_up)) + list(range(n_down))]
-            + [
-                sum(
-                    (
-                        jnp.random.randperm(n_orbitals)[:n].tolist()
-                        for n in (n_up, n_down)
-                    ),
-                    [],
-                )
-                for _ in range(n_configurations - 1)
-            ]
-        )
 
     def __call__(self, rs):
-        batch_dim, n_elec = rs.shape[:2]
-        diffs_nuc = pairwise_diffs(rs.reshape(-1, *rs.shape[2:]), self.mol.coords)
+        n_elec = rs.shape[-2]
+        diffs_nuc = pairwise_diffs(rs.reshape(-1, 3), self.mol.coords)
         aos = self.basis(diffs_nuc)
         xs = self.mo_coeff(aos)
-        xs = xs.reshape(batch_dim, 1, n_elec, -1)
+        xs = xs.reshape(-1, 1, n_elec, xs.shape[-1])
         n_up = self.n_up
-        conf_up, conf_down = self.confs[:, :n_up], self.confs[:, n_up:]
-        det_up = jnp.transpose(xs[:, :, :n_up, conf_up], axes=(0, 1, 3, 2, 4))
-        #  TODO -> .transpose(-3, -2)
-        det_down = jnp.transpose(xs[:, :, n_up:, conf_down], axes=(0, 1, 3, 2, 4))
-        # TODO -> .transpose(-3, -2)
+        conf_up, conf_down = self.confs[..., :n_up], self.confs[..., n_up:]
+        det_up = xs[..., :n_up, conf_up].swapaxes(-2, -3)
+        det_down = xs[..., n_up:, conf_down].swapaxes(-2, -3)
         sign_up, det_up = eval_log_slater(det_up)
         sign_down, det_down = eval_log_slater(det_down)
         xs = det_up + det_down
-        xs_shift = xs.reshape(batch_dim, -1).max(axis=-1)
+        xs_shift = xs.max(axis=(-2, -1))
         # the exp-normalize trick, to avoid over/underflow of the exponential
         xs_shift = jnp.where(~jnp.isinf(xs_shift), xs_shift, jnp.zeros_like(xs_shift))
         # replace -inf shifts, to avoid running into nans (see sloglindet)
-        xs = sign_up * sign_down * jnp.exp(xs - xs_shift[:, None, None])
+        xs = sign_up * sign_down * jnp.exp(xs - xs_shift[..., None, None])
         psi = self.conf_coeff(xs).squeeze(axis=-1).mean(axis=-1)
-        log_psi = jnp.log(jnp.abs(psi)) + xs_shift
-        sign_psi = jax.lax.stop_gradient(jnp.sign(psi))
+        log_psi = (jnp.log(jnp.abs(psi)) + xs_shift).squeeze()
+        sign_psi = (jax.lax.stop_gradient(jnp.sign(psi))).squeeze()
         psi = Psi(sign_psi, log_psi)
         return psi
