ec0c743c8da7c06073432a62bce4e75cdefaa529
Zeno Schätzle <zenoone@physik.fu-berlin.de>
2022-07-25 13:52:42 +0200

    add flatten and unflatten utilities

--- src/deepqmc/jax/jaxext.py
+++ src/deepqmc/jax/jaxext.py
@@ -1,2 +1,10 @@
+def flatten(x, start_axis=0):
+    return x.reshape(*x.shape[:start_axis], -1)
 
 
+def unflatten(x, axis, shape):
+    if axis < 0:
+        axis = len(x.shape) + axis
+    begin = x.shape[:axis]
+    end = x.shape[axis + 1 :]
+    return x.reshape(*begin, *shape, *end)
