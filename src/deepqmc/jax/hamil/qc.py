fb319413714f8526837f447bd800ce419db28aef
Zeno Schätzle <zenoone@physik.fu-berlin.de>
2022-07-28 17:05:42 +0200

    update local energy

--- src/deepqmc/jax/hamil/qc.py
+++ src/deepqmc/jax/hamil/qc.py
@@ -8,16 +8,20 @@ from ..utils import (
 all__ = ()
 
 
+def laplacian_flat(f):
+    return lambda r, **kwargs: laplacian(
+        lambda r, **kwargs: f(r.reshape((-1, 3)), **kwargs).log
+    )(r.flatten(), **kwargs)
+
+
 class MolecularHamiltonian:
     def __init__(self, mol):
         self.mol = mol
 
     def local_energy(self, wf, return_grad=False):
-        def loc_ene(r, mol=self.mol):
-            lap_log_psis, quantum_force = laplacian(
-                lambda x: wf(x.reshape((-1, 3)))[1]
-            )(r.flatten())
-            Es_kin = -0.5 * (lap_log_psis + (quantum_force ** 2).sum(axis=(-1)))
+        def loc_ene(r, graph_edges, mol=self.mol):
+            lap_log_psis, quantum_force = laplacian_flat(wf)(r, graph_edges=graph_edges)
+            Es_kin = -0.5 * (lap_log_psis + (quantum_force**2).sum(axis=(-1)))
             Es_nuc = nuclear_energy(mol)
             Vs_nuc = nuclear_potential(r, mol)
             Vs_el = electronic_potential(r)
