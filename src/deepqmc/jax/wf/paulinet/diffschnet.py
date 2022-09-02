e094dc45051d5f3e9c1fcc00a37124c75abdd78d
Zeno Schätzle <zenoone@physik.fu-berlin.de>
2022-09-02 11:15:42 +0200

    remove bias from gnn subnets

--- src/deepqmc/jax/wf/paulinet/diffschnet.py
+++ src/deepqmc/jax/wf/paulinet/diffschnet.py
@@ -37,7 +37,7 @@ class DiffSchNetLayer(MessagePassingLayer):
         def default_subnet_kwargs(n_layers):
             return {
                 'hidden_layers': ('log', n_layers),
-                'bias': 'not_last',
+                'bias': False,
                 'last_linear': True,
             }
 
