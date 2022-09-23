e7d638e748f4a397afc271806c31870d155e4ce6
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2022-09-23 17:08:39 +0200

    move definition of graph types to types.py

--- src/deepqmc/jax/types.py
+++ src/deepqmc/jax/types.py
@@ -1,3 +1,6 @@
 from collections import namedtuple
 
 Psi = namedtuple('Psi', 'sign log')
+GraphEdges = namedtuple('GraphEdges', 'senders receivers features')
+GraphNodes = namedtuple('GraphNodes', 'nuclei electrons')
+Graph = namedtuple('Graph', 'nodes edges')
