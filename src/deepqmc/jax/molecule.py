5ad9c8717021509f75729b4ad44538020601fd99
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2022-10-17 14:53:39 +0200

    represent molecules with a frozen dataclass
    In some parts of the Ansatz, we might need additional data about
    the molecule, like the number of spin-up electrons, that can be
    calculated from the specification of the molecule (charges, charge,
    spin). However, this calculation cannot be done inside jitted regions,
    due to limitations of jax.jit when working with bare ints or floats.
    Therefore we need to precalculate these quantities and store them
    in the Molecule object. To avoid the usual problems associated with
    these constructs, we need the Molecule object to be immutable.

--- src/deepqmc/jax/molecule.py
+++ src/deepqmc/jax/molecule.py
@@ -1,38 +1,61 @@
+from dataclasses import dataclass
 from itertools import count
 
 import jax.numpy as jnp
 
 angstrom = 1 / 0.52917721092
 
+__all__ = ['Molecule']
 
+
+@dataclass(frozen=True, init=False)
 class Molecule:
-    """Represents a molecule.
-    The array-like arguments accept anything that can be transformed to
-    :class:`torch.Tensor`.
-    Args:
-        coords (array-like, rank-2, float, a.u.): atom coordinates as rows
-        charges (array-like, rank-1, float): atom charges
-        charge (int): total charge of a molecule
-        spin (int): total spin multiplicity
-    """
+    coords: jnp.ndarray
+    charges: jnp.ndarray
+    charge: int
+    spin: int
+    data: dict
+
+    # Derived properties
+    n_nuc: int
+    n_up: int
+    n_down: int
+    n_shells: tuple
 
     # TODO
     # all_names = set(_SYSTEMS.keys())
 
     def __init__(self, coords, charges, charge, spin, unit='bohr', data=None):
-        assert len(coords) == len(charges)
-        super().__init__()
-        unit_multiplier = {'bohr': 1.0, 'angstrom': angstrom}[unit]
-        self.coords = unit_multiplier * jnp.asarray(coords)
-        self.charges = 1.0 * jnp.asarray(charges)
-        self.charge = charge
-        self.spin = spin
-        self.data = data or {}
+        def set_attr(**kwargs):
+            for k, v in kwargs.items():
+                object.__setattr__(self, k, v)
 
-        n_elec = int(self.charges.sum()) - self.charge
-        self.n_up = (n_elec + self.spin) // 2
-        self.n_down = n_elec - self.n_up
-        self.n_shells = self._n_shells()
+        unit_multiplier = {'bohr': 1.0, 'angstrom': angstrom}[unit]
+        set_attr(
+            coords=unit_multiplier * jnp.asarray(coords),
+            charges=1.0 * jnp.asarray(charges),
+            charge=charge,
+            spin=spin,
+            data=data or {},
+        )
+
+        # Derived properties
+        n_elec = int(sum(charges) - charge)
+        assert not (n_elec + spin) % 2
+        set_attr(
+            n_nuc=len(charges),
+            n_up=(n_elec + spin) // 2,
+            n_down=(n_elec - spin) // 2,
+        )
+        shells = []
+        for z in charges:
+            max_elec = 0
+            for n in count():
+                if z <= max_elec:
+                    break
+                max_elec += 2 * (1 + n) ** 2
+            shells.append(n)
+        set_attr(n_shells=tuple(shells))
 
     def __len__(self):
         return len(self.charges)
@@ -43,30 +66,17 @@ class Molecule:
     def __repr__(self):
         return (
             'Molecule(\n'
-            f'  coords={self.coords},\n'
+            f'  coords=\n{self.coords},\n'
             f'  charges={self.charges},\n'
             f'  charge={self.charge},\n'
             f'  spin={self.spin}\n'
             ')'
         )
 
-    def _n_shells(self):
-        shells = []
-        for z in self.charges:
-            max_elec = 0
-            for n in count():
-                if z <= max_elec:
-                    break
-                max_elec += 2 * (1 + n) ** 2
-            shells.append(n)
-        return tuple(shells)
-
     def as_pyscf(self):
         return [(int(charge), coord) for coord, charge in self]
 
     @property
     def n_particles(self):
-        """Returns the number of nuclei, spin-up, and spin-down electrons
-        of the molecule.
-        """
+        r"""Return the number of nuclei, spin-up, and spin-down electrons."""
         return len(self), self.n_up, self.n_down
