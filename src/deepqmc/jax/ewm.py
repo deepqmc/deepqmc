2e6db9a929b623ec138fb794a936fb8a73c1432b
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2022-11-28 11:38:02 +0100

    rewrite EWM to avoid single precision issues
    The last few local energies are stored and the exponential weighted mean
    is computed in each step explicitly (without accumulating the previous
    steps). A simple clipping is applied to remove outlier mean local energies.

--- src/deepqmc/jax/ewm.py
+++ src/deepqmc/jax/ewm.py
@@ -1,24 +1,91 @@
 from collections import namedtuple
+from math import ceil
 
 import jax
 import jax.numpy as jnp
 
 __all__ = ()
 
-EWMState = namedtuple('EWMState', 'params step mean var sqerr', defaults=4 * [None])
+EWMState = namedtuple(
+    'EWMState', 'step params buffer mean var sqerr', defaults=6 * [None]
+)
 
 
-@jax.jit
-def ewm(x=None, state=None, max_alpha=0.999, decay_alpha=10):
-    if x is None:
-        return EWMState({'max_alpha': max_alpha, 'decay_alpha': decay_alpha})
-    if state.mean is None:
-        return state._replace(step=0, mean=x, var=0, sqerr=0)
-    p = state.params
-    a = jnp.minimum(p['max_alpha'], 1 - 1 / (2 + state.step / p['decay_alpha']))
-    return state._replace(
-        step=state.step + 1,
-        mean=(1 - a) * x + a * state.mean,
-        var=(1 - a) * (x - state.mean) ** 2 + a * state.var,
-        sqerr=(1 - a) ** 2 * state.var + a**2 * state.sqerr,
+def init_ewm(
+    max_alpha=0.999,
+    decay_alpha=10,
+    window_size=None,
+    clip=1000.0,
+    max_exclude_ratio=0.0005,
+):
+    if window_size is None:
+        window_size = ceil(decay_alpha * (1 / (1 - max_alpha) - 2))
+    state = EWMState(
+        step=0,
+        params={
+            'clip': clip,
+            'max_alpha': max_alpha,
+            'decay_alpha': decay_alpha,
+            'alpha': jnp.zeros(window_size),
+        },
+        buffer=jnp.zeros(window_size),
     )
+
+    @jax.jit
+    def update(E_loc, state):
+        clip, max_alpha, decay_alpha, alpha = (
+            state.params['clip'],
+            state.params['max_alpha'],
+            state.params['decay_alpha'],
+            state.params['alpha'],
+        )
+        if state.mean is None:
+            state.params['alpha'] = state.params['alpha'].at[0].set(1.0)
+            x = jnp.nanmean(E_loc)
+            return state._replace(
+                buffer=state.buffer.at[0].set(x),
+                step=0,
+                mean=x,
+                var=jnp.array(1.0),
+                sqerr=jnp.array(1.0),
+            )
+        diff = jnp.abs(state.mean - E_loc)
+        excluded = diff > clip * state.var
+        E_loc = jnp.where(
+            excluded,
+            state.mean,
+            E_loc,
+        )
+        clip *= (
+            jnp.maximum(excluded.mean(), jnp.array(max_exclude_ratio))
+            / max_exclude_ratio
+        )
+        x = jnp.nanmean(E_loc)
+        buffer = jnp.concatenate([x[None], state.buffer[:-1]])
+        alpha = jax.lax.cond(
+            state.step + 1 >= len(alpha),
+            lambda: alpha,
+            lambda: jnp.concatenate(
+                [
+                    jnp.maximum(1 - max_alpha, 1 / (2 + state.step / decay_alpha))[
+                        None
+                    ],
+                    alpha[:-1],
+                ]
+            ),
+        )
+        beta = jnp.concatenate([jnp.array([1.0]), jnp.cumprod(1 - alpha[:-1])])
+        weights = alpha * beta
+        mean = (weights * buffer).sum()
+        variance = weights * (buffer - mean) ** 2
+        state.params['alpha'] = alpha
+        state.params['clip'] = clip
+        return state._replace(
+            step=state.step + 1,
+            buffer=buffer,
+            mean=mean,
+            var=variance.sum(),
+            sqerr=(weights * variance).sum(),
+        )
+
+    return state, update
