a7c381df66a274878529ead6036d1d66bb9078ba
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2022-11-29 11:39:14 +0100

    remove poetry, create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='deepqmc',
+    version='1.0.0',
+    author=(
+        'Jan Hermann <jan.hermann@fu-berlin.de>'
+        'Zeno Schätzle <zenoone@physik.fu-berlin.de>'
+        'Péter Bernát Szabó <peter.bernat.szabo@fu-berlin.de>'
+    ),
+    license='MIT',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'dm-haiku',
+        'e3nn-jax',
+        'h5py',
+        'hydra-core',
+        'jax[cuda]',
+        'kfac-jax',
+        'pyscf',
+        'tensorboard',
+        'toml',
+        'tqdm',
+        'uncertainties',
+    ],
+)
