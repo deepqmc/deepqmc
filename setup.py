e077586bdb56bc3027824bba4d4d29338978533c
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2022-12-01 11:55:50 +0100

    refactor requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
 from setuptools import find_packages, setup
 
-setup(
-    name='deepqmc',
-    version='1.0.0',
-    author=(
-        'Jan Hermann <jan.hermann@fu-berlin.de>'
-        'Zeno Schätzle <zenoone@physik.fu-berlin.de>'
-        'Péter Bernát Szabó <peter.bernat.szabo@fu-berlin.de>'
-    ),
-    license='MIT',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    install_requires=[
+requires = {
+    'install': {
         'dm-haiku',
         'e3nn-jax',
         'h5py',
@@ -23,10 +13,27 @@ setup(
         'toml',
         'tqdm',
         'uncertainties',
-    ],
+    },
+    'test': {'pytest', 'pytest-regressions'},
+    'format': {'black', 'flake8', 'isort'},
+}
+
+setup(
+    name='deepqmc',
+    version='1.0.0',
+    author=(
+        'Jan Hermann <jan.hermann@fu-berlin.de>, '
+        'Zeno Schätzle <zenoone@physik.fu-berlin.de>, '
+        'Péter Bernát Szabó <peter.bernat.szabo@fu-berlin.de>'
+    ),
+    license='MIT',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=requires['install'],
     scripts=['bin/deepqmc'],
-    extras=['dev'],
+    extras=['dev', 'test'],
     extras_require={
-        'dev': ['black', 'flake8', 'isort', 'pytest', 'pytest-regressions']
+        'test': requires['test'],
+        'dev': {*requires['format'], *requires['test']},
     },
 )
