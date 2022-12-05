d625450528043c645f7b138ae0d688478f5c0653
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2022-12-05 12:52:59 +0100

    [CLI] move CLI entry point into deepqmc package

--- src/deepqmc/app.py
+++ src/deepqmc/app.py
@@ -2,8 +2,10 @@ import logging
 import pickle
 import sys
 from pathlib import Path
+import warnings
 
 import jax
+import hydra
 from hydra.utils import call, get_original_cwd, to_absolute_path
 from omegaconf import OmegaConf
 from tqdm.auto import tqdm
@@ -11,10 +13,11 @@ from tqdm.auto import tqdm
 __all__ = ()
 log = logging.getLogger(__name__)
 
-
-def move_to(device):
-    jax.config.update('jax_platform_name', device)
-    log.info(f'Running on {device.upper()}')
+warnings.filterwarnings(
+    'ignore',
+    'provider=hydra.searchpath in main, path=conf is not available.',
+    UserWarning,
+)
 
 
 def instantiate_ansatz(hamil, ansatz):
@@ -31,14 +34,12 @@ def train_from_factories(hamil, ansatz, sampler, device, **kwargs):
     from .sampling import chain
     from .train import train
 
-    move_to(device)
     ansatz = instantiate_ansatz(hamil, ansatz)
     sampler = chain(*sampler[:-1], sampler[-1](hamil))
     return train(hamil, ansatz, sampler=sampler, **kwargs)
 
 
 def train_from_checkpoint(workdir, restdir, evaluate, device, chkpt='LAST', **kwargs):
-    move_to(device)
     restdir = Path(to_absolute_path(get_original_cwd())) / restdir
     if not restdir.is_dir():
         raise ValueError(f'restdir "{restdir}" is not a directory')
@@ -78,3 +79,43 @@ class TqdmStream:
             sys.stderr.write(msg)
             return 0
         return len(msg)
+
+
+def maybe_log_code_version():
+    if log.isEnabledFor(logging.DEBUG):
+        import subprocess
+
+        def git_command(command):
+            return (
+                subprocess.check_output(
+                    ['git'] + command, cwd=Path(__file__).resolve().parent
+                )
+                .strip()
+                .decode()
+            )
+
+        sha = git_command(['rev-parse', '--short', 'HEAD'])
+        diff = git_command(['diff'])
+        log.debug(f'Running with code version: {sha}')
+        if diff:
+            log.debug(f'With uncommitted changes:\n{diff}')
+
+
+def main(cfg):
+    log.info('Entering application')
+    jax.config.update('jax_platform_name', cfg.device)
+    log.info(f'Running on {cfg.device.upper()}')
+    cfg.task.workdir = str(Path.cwd())
+    log.info(f'Will work in {cfg.task.workdir}')
+    maybe_log_code_version()
+    call(cfg.task, device=cfg.device, _convert_='all')
+
+
+@hydra.main(config_path='conf', config_name='config', version_base=None)
+def cli(cfg):
+    try:
+        main(cfg)
+    except hydra.errors.InstantiationException as e:
+        raise e.__cause__ from None
+    except KeyboardInterrupt:
+        log.warning('Interrupted!')
