0ce332c8ba3489e856832cd5f02ccbd0e92cd053
Zeno Schätzle <zenoone@physik.fu-berlin.de>
2023-05-01 01:22:39 +0200

    simplify and refactor PauliNet

--- src/deepqmc/wf/paulinet/paulinet.py
+++ src/deepqmc/wf/paulinet/paulinet.py
@@ -4,7 +4,7 @@ import jax.numpy as jnp
 
 from ...physics import pairwise_diffs, pairwise_self_distance
 from ...types import Psi
-from ...utils import flatten, triu_flat, unflatten
+from ...utils import flatten, triu_flat
 from ..base import WaveFunction
 from .cusp import ElectronicAsymptotic
 from .env import ExponentialEnvelopes
@@ -46,78 +46,52 @@ def eval_log_slater(xs):
 
 class PauliNet(WaveFunction):
     r"""
-    Implement the PauliNet Ansatz from [HermannNC20]_.
-
     Args:
-        hamil (~jax.MolecularHamiltonian): the Hamiltonian of the system.
-        basis (~jax.wf.paulinet.env.ExponentialEnvelopes): optional, the orbital
-            envelopes.
-        confs (int, (:math:`N_\text{conf}`, :math:`N_\text{elec}`)): optional,
-            specifies the electronic configuration, i.e. the occupied orbitals
-            in each determinant. The value of :data:`confs[i,j]` is the index of
-            the orbital occupied by the :data:`j` th electron in the :data:`i` th
-            determinant.
-        backflow_op (Callable): optional, specifies how the backflow is applied
-            to the orbitals.
-        full_determinant (bool): optional, if :data:`False`, the determinants are
+        hamil (~MolecularHamiltonian): the Hamiltonian of the system.
+        basis (~wf.paulinet.env.ExponentialEnvelopes): the orbital envelopes.
+        backflow_op (Callable): specifies how the backflow is applied to the
+            orbitals.
+        n_determinants (int): specifies the number of determinants
+        full_determinant (bool): if :data:`False`, the determinants are
             factorized into spin-up and spin-down parts.
-        cusp_electrons (bool): optional, whether to apply the electronic cusp
+        cusp_electrons (bool): whether to apply the electronic cusp correction.
+        cusp_alpha (float): the :math:`\alpha` factor of the electronic cusp
             correction.
-        cusp_alpha (float): optional, the :math:`\alpha` factor of the electronic
-            cusp correction.
-        backflow_type (str): optional, specifies the type of backflow applied:
-
-            - ``'orbital'``: the same backflow is applied to an orbital in
-                    all determinants
-            - ``'determinant'``: separate backflows are applied to each orbital in
-                    each determinant
-        backflow_channels (int): optional, the number of chanells of backflow.
-        backflow_transform (str): optional, describes the backflow transformation.
+        backflow_transform (str): describes the backflow transformation.
             Possible values:
 
             - ``'mult'``: the backflow is a multiplicative factor
             - ``'add'``: the backflow is an additive term
             - ``'both'``: the backflow consist of a multiplicative factor
                 and an additive term
-        omni_factory (Callable): optional, creates the omni net.
-        omni_kwargs (dict): optional, extra arguments passed to
-            :data:`omni_factory`.
+        omni_factory (Callable): creates the omni net.
     """
 
     def __init__(
         self,
         hamil,
-        basis=None,
-        confs=None,
-        backflow_op=None,
         *,
-        full_determinant=False,
+        basis=None,
+        backflow_op=None,
+        n_determinants=1,
+        full_determinant=True,
         cusp_electrons=True,
         cusp_alpha=10.0,
-        backflow_channels=1,
         backflow_transform='mult',
         omni_factory=None,
-        omni_kwargs=None,
-        mo_scaling: float = 1.0,
     ):
         super().__init__(hamil.mol)
+        basis = basis or ExponentialEnvelopes.from_mol
+        backflow_op = BackflowOp
+        omni_factory = OmniNet
         n_up, n_down = self.n_up, self.n_down
-        confs = (
-            (
-                [list(range(n_up)) + list(range(n_down))]
-                if not full_determinant
-                else [list(range(n_up + n_down)) * 2]
-            )
-            if confs is None
-            else confs
-        )
-        self.n_orbitals = max(sum(confs, [])) + 1
-        self.confs = jnp.array(confs)
-        self.basis = basis or ExponentialEnvelopes.from_mol(hamil.mol)
+        self.n_det = n_determinants
+        self.full_determinant = full_determinant
+        self.basis = basis(hamil.mol)
         self.mo_coeff = hk.Linear(
-            self.n_orbitals,
+            n_determinants * (n_up + n_down),
             with_bias=False,
-            w_init=lambda s, d: hk.initializers.VarianceScaling(mo_scaling)(s, d)
+            w_init=lambda s, d: hk.initializers.VarianceScaling(1.0)(s, d)
             + jnp.ones(s),
             name='mo_coeff',
         )
@@ -129,21 +103,14 @@ class PauliNet(WaveFunction):
             if cusp_electrons
             else (None, None)
         )
-        self.n_determinants = len(self.confs) * backflow_channels
-        self.full_determinant = full_determinant
         backflow_spec = [
             *((n_up + n_down, n_up + n_down) if full_determinant else (n_up, n_down)),
-            self.n_determinants,
+            n_determinants,
             2 if backflow_transform == 'both' else 1,
         ]
-        if backflow_transform == 'both':
-            backflow_spec[1] *= 2
         self.backflow_transform = backflow_transform
-        self.backflow_op = backflow_op or BackflowOp()
-
-        omni_factory = omni_factory or OmniNet
-        self.omni = omni_factory(hamil.mol, *backflow_spec, **(omni_kwargs or {}))
-        self.full_determinant = full_determinant
+        self.backflow_op = backflow_op()
+        self.omni = omni_factory(hamil.mol, *backflow_spec)
 
     def _backflow_op(self, xs, fs, dists_nuc):
         if self.backflow_transform == 'mult':
@@ -159,63 +126,52 @@ class PauliNet(WaveFunction):
         return self.backflow_op(xs, fs_mult, fs_add, dists_nuc)
 
     def __call__(self, phys_conf, return_mos=False):
-        n_elec = phys_conf.r.shape[-2]
         diffs_nuc = pairwise_diffs(phys_conf.r, phys_conf.R)
-        dists_nuc = jnp.sqrt(diffs_nuc[..., -1]).reshape(
-            -1, n_elec, self.mol.n_particles[0]
-        )
+        dists_nuc = jnp.sqrt(diffs_nuc[..., -1])
         dists_elec = pairwise_self_distance(phys_conf.r, full=True)
         aos = self.basis(diffs_nuc)
-        xs = self.mo_coeff(aos)
-        xs = jnp.expand_dims(xs, axis=-3)
-        J, fs = self.omni(phys_conf) if self.omni else (None, None)
+        orb = self.mo_coeff(aos)
+        jastrow, fs = self.omni(phys_conf) if self.omni else (None, None)
+        orb_up, orb_down = (
+            (orb, orb)
+            if self.full_determinant
+            else jnp.split(orb, (self.n_up * self.n_det,), axis=-1)
+        )
+        orb_up = orb_up[: self.n_up]
+        orb_down = orb_down[self.n_up :]
         if fs is not None:
-            orb_up = self._backflow_op(
-                xs[:, : self.n_up], fs[0], dists_nuc[: self.n_up]
-            )
-            orb_down = self._backflow_op(
-                xs[:, self.n_up :], fs[1], dists_nuc[self.n_up :]
-            )
-            xs = jnp.concatenate((orb_up, orb_down), axis=1)
-        n_up = self.n_up
-        n_slice = n_up + self.n_down if self.full_determinant else n_up
-        conf_up, conf_down = self.confs[..., :n_slice], self.confs[..., n_slice:]
-        det_up = xs[..., :n_up, conf_up].swapaxes(-2, -3)
-        det_down = xs[..., n_up:, conf_down].swapaxes(-2, -3)
-        if self.full_determinant:
-            det_full = jnp.zeros((*det_up.shape[:-2], n_elec, n_elec))
-            det_full = det_full.at[..., :n_up, :].set(det_up)
-            det_full = det_full.at[..., n_up:, :].set(det_down)
-            det_up = det_full
-            det_down = jnp.empty((*det_down.shape[:-2], 0, 0))
+            orb_up = self._backflow_op(orb_up, fs[0], dists_nuc[: self.n_up])
+            orb_down = self._backflow_op(orb_down, fs[1], dists_nuc[self.n_up :])
+        orb_up = orb_up.reshape(self.n_up, self.n_det, -1).swapaxes(-2, -3)
+        orb_down = orb_down.reshape(self.n_down, self.n_det, -1).swapaxes(-2, -3)
         if return_mos:
-            return det_up, det_down
-        sign_up, det_up = eval_log_slater(det_up)
-        sign_down, det_down = eval_log_slater(det_down)
-        xs = det_up + det_down
-        xs_shift = xs.max(axis=(-2, -1))
+            return orb_up, orb_down
+
+        if self.full_determinant:
+            sign, xs = eval_log_slater(jnp.concatenate([orb_up, orb_down], axis=-2))
+        else:
+            sign_up, det_up = eval_log_slater(orb_up)
+            sign_down, det_down = eval_log_slater(orb_down)
+            sign, xs = sign_up * sign_down, det_up + det_down
+
+        xs_shift = xs.max()
         # the exp-normalize trick, to avoid over/underflow of the exponential
         xs_shift = jnp.where(~jnp.isinf(xs_shift), xs_shift, jnp.zeros_like(xs_shift))
         # replace -inf shifts, to avoid running into nans (see sloglindet)
-        xs = sign_up * sign_down * jnp.exp(xs - xs_shift[..., None, None])
-        psi = self.conf_coeff(xs).squeeze(axis=-1).mean(axis=-1)
+        xs = sign * jnp.exp(xs - xs_shift)
+        psi = self.conf_coeff(xs).squeeze()
         log_psi = jnp.log(jnp.abs(psi)) + xs_shift
         sign_psi = jax.lax.stop_gradient(jnp.sign(psi))
         if self.cusp_same:
             cusp_same = self.cusp_same(
                 jnp.concatenate(
-                    [
-                        triu_flat(dists_elec[..., idxs, idxs])
-                        for idxs in self.spin_slices
-                    ],
+                    [triu_flat(dists_elec[idxs, idxs]) for idxs in self.spin_slices],
                     axis=-1,
                 )
             )
-            cusp_anti = self.cusp_anti(
-                flatten(dists_elec[..., :n_up, n_up:], start_axis=-2)
-            )
+            cusp_anti = self.cusp_anti(flatten(dists_elec[: self.n_up, self.n_up :]))
             log_psi = log_psi + cusp_same + cusp_anti
-        if J is not None:
-            log_psi = log_psi + J
+        if jastrow is not None:
+            log_psi = log_psi + jastrow
 
-        return Psi(sign_psi.squeeze(), log_psi.squeeze())
+        return Psi(sign_psi, log_psi)
