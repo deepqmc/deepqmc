b22dd0679f598c311e187b296541edba048cb7c1
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2023-05-10 14:08:32 +0200

    add option for isotropic envelope

--- src/deepqmc/wf/paulinet/env.py
+++ src/deepqmc/wf/paulinet/env.py
@@ -7,34 +7,35 @@ from ...utils import norm
 class ExponentialEnvelopes(hk.Module):
     r"""Create exponential envelopes centered on the nuclei."""
 
-    def __init__(self, shells):
+    def __init__(self, shells, *, isotropic):
         super().__init__()
-        center_idx, zetas = zip(*shells)
-        self.center_idx = jnp.array(center_idx)
+        center_idx, zetas = map(jnp.array, zip(*shells))
+        self.center_idx = center_idx
+        if not isotropic:
+            zetas = zetas[..., None, None] * jnp.eye(3)
         self.zetas = hk.get_parameter(
             'zetas',
-            jnp.array(zetas).shape,
-            init=lambda shape, dtype: jnp.array(zetas),
+            zetas.shape,
+            init=lambda shape, dtype: zetas,
         )
+        self.isotropic = isotropic
 
     def __call__(self, diffs):
-        return jnp.exp(
-            -norm(
-                jnp.einsum(
-                    'ers,ies->ier', self.zetas, diffs[..., self.center_idx, :-1]
-                ),
-                safe=True,
-            )
+        d = diffs[..., self.center_idx, :-1]
+        R = (
+            self.zetas * d
+            if self.isotropic
+            else jnp.einsum('ers,ies->ier', self.zetas, d)
         )
+        return jnp.exp(-norm(R, safe=True))
 
     @classmethod
-    def from_mol(cls, mol):
+    def from_mol(cls, mol, **kwargs):
         r"""Create the input of the constructor from a :class:`~deepqmc.Molecule`."""
         shells = []
         for i, (z, n_shell, n_pp_shell) in enumerate(
             zip(mol.charges, mol.n_shells, mol.n_pp_shells)
         ):
             for k in range(n_pp_shell, n_shell):
-                #  shells.append((i, [[z / (k + 1)] * 3] * 3))
-                shells.append((i, z / (k + 1) * jnp.eye(3)))
-        return cls(shells)
+                shells.append((i, z / (k + 1)))
+        return cls(shells, **kwargs)
