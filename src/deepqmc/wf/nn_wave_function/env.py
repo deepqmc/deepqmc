6c1fe9c783a5a84e81b197403dc025478497ace3
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2023-05-26 13:45:43 +0200

    implement spin unrestricted envelopes

--- src/deepqmc/wf/nn_wave_function/env.py
+++ src/deepqmc/wf/nn_wave_function/env.py
@@ -8,7 +8,14 @@ class ExponentialEnvelopes(hk.Module):
     r"""Create exponential envelopes centered on the nuclei."""
 
     def __init__(
-        self, mol, n_determinants, *, isotropic, per_shell, per_orbital_exponent
+        self,
+        mol,
+        n_determinants,
+        *,
+        isotropic,
+        per_shell,
+        per_orbital_exponent,
+        spin_restricted,
     ):
         super().__init__()
         shells = []
@@ -18,36 +25,60 @@ class ExponentialEnvelopes(hk.Module):
             for k in range(n_pp_shell, n_shell if per_shell else n_pp_shell + 1):
                 shells.append((i, z / (k + 1)))
         self.center_idx, zetas = map(jnp.array, zip(*shells))  # [n_env]
-        self.pi = hk.get_parameter(
-            'pi',
-            (n_determinants * (mol.n_up + mol.n_down), len(zetas)),
-            init=lambda s, d: hk.initializers.VarianceScaling(1.0)(s, d) + jnp.ones(s),
-        )  # [n_orb, n_env]
+        self.pi = [
+            _get_pi_for_one_spin(name, n_determinants, mol.n_up, mol.n_down, len(zetas))
+            for name in (['pi'] if spin_restricted else ['pi_up', 'pi_down'])
+        ]  # [n_orb, n_env]
         if per_orbital_exponent:
             zetas = jnp.tile(
                 zetas[None], (n_determinants * (mol.n_up + mol.n_down), 1)
             )  # [n_orb, n_env]
         if not isotropic:
             zetas = zetas[..., None, None] * jnp.eye(3)
-        self.zetas = hk.get_parameter(
-            'zetas',
-            zetas.shape,
-            init=lambda shape, dtype: zetas,
-        )  # [n_env] or [n_orb, n_env] or [n_env, 3, 3] or [n_orb, n_env, 3, 3]
+        self.zetas = [
+            _get_zeta_for_one_spin(name, zetas)
+            for name in (['zetas'] if spin_restricted else ['zetas_up', 'zetas_down'])
+        ]  # [n_env] or [n_orb, n_env] or [n_env, 3, 3] or [n_orb, n_env, 3, 3]
         self.isotropic = isotropic
         self.per_orbital_exponent = per_orbital_exponent
+        self.spin_restricted = spin_restricted
+        self.n_up = mol.n_up
 
-    def __call__(self, diffs):
+    def _call_for_one_spin(self, zeta, pi, diffs):
         d = diffs[..., self.center_idx, :-1]
         if self.isotropic:
             d = norm(d, safe=True)  # [n_el, n_env]
             if self.per_orbital_exponent:
                 d = d[:, None]  # [n_el, 1, n_env]
-            exponent = jnp.abs(self.zetas * d)  # [n_el, n_env] or [n_el, n_orb, n_env]
+            exponent = jnp.abs(zeta * d)  # [n_el, n_env] or [n_el, n_orb, n_env]
         else:
             exponent = norm(
-                jnp.einsum('...ers,ies->i...er', self.zetas, d), safe=True
+                jnp.einsum('...ers,ies->i...er', zeta, d), safe=True
             )  # [n_el, n_env] or [n_el, n_orb, n_env]
         if not self.per_orbital_exponent:
             exponent = exponent[:, None]  # [n_el, 1, n_env]
-        return (self.pi * jnp.exp(-exponent)).sum(axis=-1)  # [n_el, n_orb]
+        return (pi * jnp.exp(-exponent)).sum(axis=-1)  # [n_el, n_orb]
+
+    def __call__(self, diffs):
+        if self.spin_restricted:
+            return self._call_for_one_spin(self.zetas[0], self.pi[0], diffs)
+        else:
+            orbs = [
+                self._call_for_one_spin(zeta, pi, diff)
+                for zeta, pi, diff in zip(
+                    self.zetas, self.pi, jnp.split(diffs, (self.n_up,))
+                )
+            ]
+            return jnp.concatenate(orbs)
+
+
+def _get_pi_for_one_spin(name, n_determinants, n_up, n_down, n_env):
+    return hk.get_parameter(
+        name,
+        (n_determinants * (n_up + n_down), n_env),
+        init=lambda s, d: hk.initializers.VarianceScaling(1.0)(s, d) + jnp.ones(s),
+    )
+
+
+def _get_zeta_for_one_spin(name, zeta):
+    return hk.get_parameter(name, zeta.shape, init=lambda shape, dtype: jnp.copy(zeta))
