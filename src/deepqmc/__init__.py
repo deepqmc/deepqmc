5ff9222111971f77e140c700c39b53002711417f
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2023-06-05 13:43:54 +0200

    fix tests and config files

--- src/deepqmc/__init__.py
+++ src/deepqmc/__init__.py
@@ -1,7 +1,17 @@
+import jax  # noqa: F401
+from omegaconf import OmegaConf
+
 from .hamil import MolecularHamiltonian
 from .molecule import Molecule
 from .sampling import DecorrSampler, MetropolisSampler, ResampledSampler
 from .train import train
+
+
+def eval_resolver(x):
+    return eval(x)
+
+
+OmegaConf.register_new_resolver('eval', eval_resolver)
 
 __all__ = [
     'DecorrSampler',
