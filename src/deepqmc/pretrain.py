4c1363299feaeb9c5a1f70c8ae0d2c1c9644854e
Zeno Schätzle <zenoone@physik.fu-berlin.de>
2023-06-07 15:32:21 +0200

    update pretraining

--- src/deepqmc/pretrain.py
+++ src/deepqmc/pretrain.py
@@ -24,9 +24,6 @@ def pretrain(  # noqa: C901
 
     Args:
         rng (~deepqmc.types.RNGSeed): key used for PRNG.
-        hamil (~deepqmc.hamil.Hamiltonian): the Hamiltonian of the physical system.
-        ansatz (~deepqmc.wf.WaveFunction): the wave function Ansatz.
-        opt (``optax`` optimizer): the optimizer to use.
         sampler (~deepqmc.sampling.Sampler): the sampler instance to use.
         steps: an iterable yielding the step numbers for the pretraining.
         sample_size (int): the number of samples to use in a batch.
@@ -37,31 +34,30 @@ def pretrain(  # noqa: C901
 
     @hk.without_apply_rng
     @hk.transform
-    def baseline(phys_config, return_mos=False):
-        return Baseline(hamil.mol, *baseline_init)(phys_config, return_mos)
+    def baseline(phys_conf):
+        return Baseline(hamil.mol, None, **baseline_init)(phys_conf)
 
     init_pc = hamil.init_sample(rng, sampler.mols[0].coords, 1)[0]
     params = ansatz.init(rng, init_pc, False)
-    params_baseline = baseline.init(rng, init_pc, False)
+    params_baseline = baseline.init(rng, init_pc)
     baseline = partial(baseline.apply, params_baseline)
 
     def loss_fn(params, phys_config):
-        n_batch = phys_config.r.shape[0]
-        dets = jax.vmap(ansatz.apply, (None, 0, None))(params, phys_config, True)
-        dets_target = jax.vmap(baseline, (0, None))(phys_config, True)
-        repeats = math.ceil(dets[0].shape[-3] / dets_target[0].shape[-3])
-        dets_target = (
-            jnp.tile(det, (1, repeats, 1, 1))[:, : dets[0].shape[-3]]
-            for det in dets_target
-        )
-        mos, mos_target = (
-            jnp.concatenate(
-                (det_up.reshape(n_batch, -1), det_down.reshape(n_batch, -1)), axis=-1
+        orbs = jax.vmap(ansatz.apply, (None, 0, None))(params, phys_config, True)
+        _, n_det, n_up, n_orb_up = orbs[0].shape
+        target = jax.vmap(baseline)(phys_config)
+        n_det_target = target.shape[-3]
+        target = jnp.tile(target, (math.ceil(n_det / n_det_target), 1, 1))[:, :n_det]
+        # if the baseline has fewer determinatns than the ansatz targets are repeated
+        target = (target[..., :n_up, :n_up], target[..., n_up:, n_up:])
+        if n_orb_up != n_up:
+            target = (
+                jnp.concatenate((target[0], jnp.zeros_like(target[0])), axis=-1),
+                jnp.concatenate((jnp.zeros_like(target[1]), target[1]), axis=-1),
             )
-            for det_up, det_down in (dets, dets_target)
-        )
-        losses = (mos - mos_target) ** 2
-        return losses.mean(), losses.mean(axis=-1)
+        # in full determinant mode off diagonal elements are pretrained against zero
+        loss = sum((o - t) ** 2 for o, t in zip(orbs, target))
+        return loss.mean(), loss.mean(axis=(-3, -2, -1))
 
     loss_and_grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
 
@@ -77,18 +73,18 @@ def pretrain(  # noqa: C901
     else:
         raise NotImplementedError
 
-    smpl_state = sampler.init(rng, baseline, sample_size)
+    smpl_state = sampler.init(rng, partial(ansatz.apply, params), sample_size)
     opt_state = opt.init(params)
 
     @jax.jit
-    def sample_wf(state, rng, select_idxs):
-        return sampler.sample(rng, state, baseline, select_idxs)
+    def sample_wf(state, rng, params, select_idxs):
+        return sampler.sample(rng, state, partial(ansatz.apply, params), select_idxs)
 
     for step, rng in zip(steps, hk.PRNGSequence(rng)):
         rng, rng_sample = jax.random.split(rng)
         select_idxs = sampler.select_idxs(sample_size, step)
         smpl_state, phys_config, smpl_stats = sample_wf(
-            smpl_state, rng_sample, select_idxs
+            smpl_state, rng_sample, params, select_idxs
         )
 
         params, opt_state, losses = _step(
