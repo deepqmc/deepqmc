b21169ed9802980de7f769f779970dd972d96501
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2023-06-07 09:29:04 +0200

    update tests

--- tests/test_gnn.py
+++ tests/test_gnn.py
@@ -27,10 +27,8 @@ class TestGraph:
         mask_vals,
         ndarrays_regression,
     ):
-        graph_edges = GraphEdgeBuilder(
-            mask_self, offsets, mask_vals
-        )(nodes, nodes)
-        ndarrays_regression.check(helpers.flatten_pytree(graph_edges))
+        graph_edges = GraphEdgeBuilder(mask_self, offsets, mask_vals)(nodes, nodes)
+        ndarrays_regression.check({'graph_edges': graph_edges})
 
     def test_molecular_graph_edge_builder(self, helpers, ndarrays_regression):
         mol = helpers.mol()
@@ -40,7 +38,7 @@ class TestGraph:
             *mol.n_particles,
             edge_types,
         )(phys_conf)
-        ndarrays_regression.check(helpers.flatten_pytree(graph_edges))
+        ndarrays_regression.check(graph_edges)
 
 
 class TestGNN:
