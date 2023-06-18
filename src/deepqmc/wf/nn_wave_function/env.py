1739e1c88376a778d0ada74cd25c6ca22bc19598
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2023-06-18 17:02:20 +0200

    add softplus_zeta option
    Instead of taking the absolute value of the envelope exponents,
    DeepErwin passes the `zeta` parameter through `softplus`. This means
    that by default KFAC can't recognize this layer as shift_and_scale. The
    manual registration of this layer is not implemented for us, e.g. if
    `softplus_zeta=True`, the zeta parameter will be `Orphan` with KFAC.

--- src/deepqmc/wf/nn_wave_function/env.py
+++ src/deepqmc/wf/nn_wave_function/env.py
@@ -1,5 +1,6 @@
 import haiku as hk
 import jax.numpy as jnp
+from jax.nn import softplus
 
 from ...physics import pairwise_diffs
 from ...utils import norm, unflatten
@@ -17,6 +18,7 @@ class ExponentialEnvelopes(hk.Module):
         per_shell,
         per_orbital_exponent,
         spin_restricted,
+        softplus_zeta,
     ):
         super().__init__()
         shells = []
@@ -45,6 +47,7 @@ class ExponentialEnvelopes(hk.Module):
         self.spin_restricted = spin_restricted
         self.n_up = mol.n_up
         self.n_det = n_determinants
+        self.softplus_zeta = softplus_zeta
 
     def _call_for_one_spin(self, zeta, pi, diffs):
         d = diffs[..., self.center_idx, :-1]
@@ -52,7 +55,9 @@ class ExponentialEnvelopes(hk.Module):
             d = norm(d, safe=True)  # [n_el, n_env]
             if self.per_orbital_exponent:
                 d = d[:, None]  # [n_el, 1, n_env]
-            exponent = jnp.abs(zeta * d)  # [n_el, n_env] or [n_el, n_orb, n_env]
+            exponent = (
+                (softplus(zeta) * d) if self.softplus_zeta else jnp.abs(zeta * d)
+            )  # [n_el, n_env] or [n_el, n_orb, n_env]
         else:
             exponent = norm(
                 jnp.einsum('...ers,ies->i...er', zeta, d), safe=True
