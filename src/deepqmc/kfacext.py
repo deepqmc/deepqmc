3fba3e3c6c78d1d82955f25ced44d438e3fcd7d1
Zeno Schätzle <zenoone@physik.fu-berlin.de>
2023-06-26 12:17:25 +0200

    temporarily monkey patch kfac

--- src/deepqmc/kfacext.py
+++ src/deepqmc/kfacext.py
@@ -1,8 +1,15 @@
 import logging
+from typing import Tuple
 
 import jax.numpy as jnp
 import kfac_jax
-from jax import vmap
+from jax import lax, vmap
+from kfac_jax._src.utils import (
+    get_special_case_zero_inv,
+    psd_inv_cholesky,
+    psd_matrix_norm,
+    types,
+)
 
 __all__ = ['make_graph_patterns']
 
@@ -134,3 +141,115 @@ def make_graph_patterns():
         *kfac_jax.tag_graph_matcher.DEFAULT_GRAPH_PATTERNS,
     )
     return graph_patterns
+
+
+Array = types.Array
+Numeric = types.Numeric
+
+
+def pi_adjusted_kronecker_inverse(
+    *arrays: Array,
+    damping: Numeric,
+) -> Tuple[Array, ...]:
+    """Computes pi-adjusted factored damping inverses.
+
+    The inverse of `a_1 kron a_2 kron ... kron a_n + damping * I` is not Kronecker
+    factored in general, because of the added identity. [1] proposed a pi-adjusted
+    factored damping approach to approximate the inverse as a Kronecker product.
+    [2] generalized this approach from two to tree factors, and [3] generalized it
+    to arbitrary numbers of factors. This function implements the generalized
+    approach.
+
+    [1] - https://arxiv.org/abs/1503.05671
+    [2] - https://openreview.net/forum?id=SkkTMpjex
+    [3] - https://ui.adsabs.harvard.edu/abs/2021arXiv210602925R/abstract
+
+    Args:
+      *arrays: A list of matrices, vectors (which are interpreted
+        as representing the diagonal of a matrix) or scalars (which are
+        interpreted as being a 1x1 matrix). All matrices must be PSD.
+      damping: The weight of the identity added to the Kronecker product.
+
+    Returns:
+      A list of factors with the same length as the input `arrays` whose Kronecker
+      product approximates the inverse of `a_1 kron ... kron a_n + damping * I`.
+    """
+    # The implementation writes each single factor as `c_i u_i`, where the matrix
+    # `u_i` is such that `trace(u_i) / dim(u_i) = 1`. We then factor out all the
+    # scalar factors `c_i` into a single overall scaling coefficient and
+    # distribute the damping to each single non-scalar factor `u_i` equally before
+    # inverting them.
+
+    norm_type = 'avg_trace'
+
+    norms = [psd_matrix_norm(a, norm_type=norm_type) for a in arrays]
+
+    # Compute the normalized factors `u_i`, such that Trace(u_i) / dim(u_i) = 1
+    us = [ai / ni for ai, ni in zip(arrays, norms)]
+
+    # kron(arrays) = c * kron(us)
+
+    c = jnp.prod(jnp.array(norms))
+
+    damping = damping.astype(
+        c.dtype
+    )  # pytype: disable=attribute-error  # numpy-scalars
+
+    def regular_inverse() -> Tuple[Array, ...]:
+        non_scalars = sum(1 if a.size != 1 else 0 for a in arrays)
+
+        # We distribute the overall scale over each factor, including scalars
+        if non_scalars == 0:
+            # In the case where all factors are scalar we need to add the damping
+            c_k = jnp.power(c + damping, 1.0 / len(arrays))
+        else:
+            # We distribute the damping only inside the non-scalar factors
+            d_hat = jnp.power(damping / c, 1.0 / non_scalars)
+            c_k = jnp.power(c, 1.0 / len(arrays))
+
+        u_hats_inv = []
+
+        for u in us:
+            if u.size == 1:
+                inv = jnp.ones_like(u)  # damping not used in the scalar factors
+
+            elif u.ndim == 2:
+                inv = psd_inv_cholesky(u, d_hat)
+
+            else:  # diagonal case
+                assert u.ndim == 1
+                inv = 1.0 / (u + d_hat)
+
+            u_hats_inv.append(inv / c_k)
+
+        return tuple(u_hats_inv)
+
+    def zero_inverse() -> Tuple[Array, ...]:
+        # In the special case where for some reason one of the factors is zero, then
+        # the inverse is just `damping^-1 * I`, hence we write each factor as
+        # `damping^(1/k) * I`.
+
+        c_k = jnp.power(damping, 1.0 / len(arrays))
+
+        u_hats_inv = []
+
+        for u in us:
+            if u.ndim == 2:
+                inv = jnp.eye(u.shape[0], dtype=u.dtype)
+
+            else:
+                inv = jnp.ones_like(u)
+
+            u_hats_inv.append(inv / c_k)
+
+        return tuple(u_hats_inv)
+
+    if get_special_case_zero_inv():
+        return lax.cond(jnp.greater(c, 0.0), regular_inverse, zero_inverse)
+
+    else:
+        return regular_inverse()
+
+
+# temporary monkey patch until kfac works for layers with shape 1
+kfac_jax.utils.pi_adjusted_kronecker_inverse = pi_adjusted_kronecker_inverse
