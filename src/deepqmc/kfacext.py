03bc88545b332ef87875632c8d424598ee1acb01
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2023-06-28 11:03:48 +0200

    match kfacext to FermiNet curvature_tags_and_blocks

--- src/deepqmc/kfacext.py
+++ src/deepqmc/kfacext.py
@@ -16,40 +16,6 @@ __all__ = ['make_graph_patterns']
 log = logging.getLogger(__name__)
 
 
-class DenseBlock(kfac_jax.DenseTwoKroneckerFactored):
-    r"""
-    Modification of the kfac_jax dense block.
-
-    Expand the input to include batch dimension, if necessary.
-    """
-
-    def _update_curvature_matrix_estimate(
-        self,
-        state,
-        estimation_data,
-        ema_old,
-        ema_new,
-        batch_size,
-    ):
-        (x,) = estimation_data['inputs']
-        (dy,) = estimation_data['outputs_tangent']
-        if not kfac_jax.utils.first_dim_is_size(batch_size, x, dy):
-            log.debug("Input of dense block doesn't have first dim of batch_size")
-            log.debug(f"It's shape is {x.shape}, expanding to {(batch_size, *x.shape)}")
-            x, dy = (
-                jnp.tile(a[None], (batch_size, *(1 for _ in a.shape))).reshape(
-                    (-1, a.shape[-1])
-                )
-                for a in (x, dy)
-            )
-            batch_size = x.size // x.shape[-1]
-            estimation_data['inputs'], estimation_data['outputs_tangent'] = (x,), (dy,)
-
-        return super()._update_curvature_matrix_estimate(
-            state, estimation_data, ema_old, ema_new, batch_size
-        )
-
-
 class RepeatedDenseBlock(kfac_jax.DenseTwoKroneckerFactored):
     """Dense block that is repeatedly applied to multiple inputs (e.g. vmap)."""
 
@@ -112,7 +78,7 @@ def make_dense_pattern(
             if with_bias
             else f'repeated{n_extra_dims}_dense_no_bias'
         ),
-        tag_primitive=kfac_jax.LayerTag(f'repeated{n_extra_dims}_dense_tag', 1, 1),
+        tag_primitive=kfac_jax.LayerTag(f'repeated_dense_tag', 1, 1),
         #  precedence=0 if with_bias else 1,
         compute_func=compute_func,
         parameters_extractor_func=_dense_parameter_extractor,
@@ -130,11 +96,8 @@ def make_graph_patterns():
                     with_bias, extra_dims=tuple(8 + i for i in range(n_extra_dims))
                 )
             )
-            kfac_jax.set_default_tag_to_block_ctor(
-                f'repeated{n_extra_dims}_dense_tag', RepeatedDenseBlock
-            )
 
-    kfac_jax.set_default_tag_to_block_ctor('dense_tag', DenseBlock)
+    kfac_jax.set_default_tag_to_block_ctor('repeated_dense_tag', RepeatedDenseBlock)
 
     graph_patterns = (
         *(custom_patterns),
