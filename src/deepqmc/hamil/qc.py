03e0c4bd8f8765e431fa3d43d259fcced4696c77
Szabo Bernat <peter.bernat.szabo@fu-berlin.de>
2023-07-28 17:00:53 +0200

    improve sample initialization

--- src/deepqmc/hamil/qc.py
+++ src/deepqmc/hamil/qc.py
@@ -1,8 +1,6 @@
-from functools import partial
-
 import jax.numpy as jnp
 import jax_dataclasses as jdc
-from jax import random, vmap
+from jax import lax, random, vmap
 
 from ..physics import (
     electronic_potential,
@@ -58,71 +56,86 @@ class MolecularHamiltonian(Hamiltonian):
         """
 
         Rs = jnp.tile(Rs[None], (n, 1, 1)) if Rs.ndim == 2 else Rs
+        return vmap(self.init_single_sample)(random.split(rng, n), Rs)
+
+    def init_single_sample(self, rng, R):
         rng_remainder, rng_normal, rng_spin = random.split(rng, 3)
         valence_electrons = self.mol.ns_valence - self.mol.charge / self.mol.n_nuc
-        base = jnp.floor(valence_electrons).astype(jnp.int32)
-        prob = valence_electrons - base
-        electrons_of_atom = jnp.tile(base[None], (n, 1))
-        n_remainder = int(self.mol.ns_valence.sum() - self.mol.charge - base.sum())
-        idxs = jnp.tile(
-            jnp.concatenate(
-                [i_atom * jnp.ones(b, jnp.int32) for i_atom, b in enumerate(base)]
-            )[None],
-            (n, 1),
-        )
-        if n_remainder > 0:
-            extra = random.categorical(rng_remainder, prob, shape=(n, n_remainder))
-            n_extra = vmap(partial(jnp.bincount, length=base.shape[-1]))(extra)
-            electrons_of_atom += n_extra
-        idxs = []
-        rng_spin = random.split(rng_spin, len(electrons_of_atom))
-        for rng_spin, elec_of_atom, R in zip(rng_spin, electrons_of_atom, Rs):
-            up, down = self.distribute_spins(rng_spin, R, elec_of_atom)
-            idxs.append(
-                jnp.concatenate(
-                    [
-                        i_atom * jnp.ones(n_up, jnp.int32)
-                        for i_atom, n_up in enumerate(up)
-                    ]
-                    + [
-                        i_atom * jnp.ones(n_down, jnp.int32)
-                        for i_atom, n_down in enumerate(down)
-                    ]
-                )
+        electrons_of_atom = jnp.floor(valence_electrons).astype(jnp.int32)
+
+        def cond_fn(value):
+            _, electrons_of_atom = value
+            return (
+                self.mol.ns_valence.sum() - self.mol.charge - electrons_of_atom.sum()
+                > 0
             )
-        idxs = jnp.stack(idxs)
-        centers = Rs[jnp.broadcast_to(jnp.arange(n)[:, None], idxs.shape), idxs]
-        std = (elec_std or self.elec_std) * jnp.sqrt(self.mol.charges)[idxs][..., None]
-        rs = centers + std * random.normal(rng_normal, centers.shape)
-        return PhysicalConfiguration(Rs, rs, jnp.zeros(n, dtype=jnp.int32))
+
+        def body_fn(value):
+            rng, electrons_of_atom = value
+            rng, rng_categorical = random.split(rng)
+            atom_idx = random.categorical(
+                rng_categorical, valence_electrons - electrons_of_atom, shape=()
+            )
+            electrons_of_atom = electrons_of_atom.at[atom_idx].add(1)
+            return rng, electrons_of_atom
+
+        _, electrons_of_atom = lax.while_loop(
+            cond_fn, body_fn, (rng_remainder, electrons_of_atom)
+        )
+        rng_spin = random.split(rng_spin, 1)
+        up, down = self.distribute_spins(rng_spin, R, electrons_of_atom)
+        up = (jnp.cumsum(up)[:, None] <= jnp.arange(self.mol.n_up)).sum(axis=0)
+        down = (jnp.cumsum(down)[:, None] <= jnp.arange(self.mol.n_down)).sum(axis=0)
+        idxs = jnp.concatenate([up, down])
+        centers = R[idxs]
+        r = centers + random.normal(rng_normal, centers.shape)
+        return PhysicalConfiguration(R, r, jnp.array(0))
 
     def distribute_spins(self, rng, R, elec_of_atom):
         up, down = jnp.zeros_like(elec_of_atom), jnp.zeros_like(elec_of_atom)
         # try to distribute electron pairs evenly across atoms
-        for i in range(jnp.max(elec_of_atom)):
+
+        def pair_cond_fn(value):
+            i, *_ = value
+            return i < jnp.max(elec_of_atom)
+
+        def pair_body_fn(value):
+            i, up, down = value
             mask = elec_of_atom >= 2 * (i + 1)
-            if mask.sum() <= self.mol.n_down - down.sum():
-                up = up.at[mask].add(1)
-                down = down.at[mask].add(1)
+            increment = jnp.where(
+                mask & (mask.sum() + down.sum() <= self.mol.n_down), 1, 0
+            )
+            up = up + increment
+            down = down + increment
+            return i + 1, up, down
+
+        _, up, down = lax.while_loop(pair_cond_fn, pair_body_fn, (0, up, down))
 
         # distribute remaining electrons such that opposite spin electrons
         # end up close in an attempt to mimic covalent bonds
-        nearest_neighbor_indices = jnp.argsort(pairwise_distance(R, R))
-        center = None
-        i = 0
-        while (elec_of_atom - up - down).sum():
-            if center is None:
-                center = argmax_random_choice(rng, elec_of_atom - up - down)
-            else:
-                ordered = nearest_neighbor_indices[center]
-                center = ordered[(elec_of_atom - up - down)[ordered] > 0][0]
-            if i % 2 and self.mol.n_down - down.sum() > 0:
-                down = down.at[center].add(1)
-            else:
-                up = up.at[center].add(1)
-            i += 1
-        assert up.sum() == self.mol.n_up
-        assert down.sum() == self.mol.n_down
+        dists = pairwise_distance(R, R).at[jnp.diag_indices(len(R))].set(jnp.inf)
+        nearest_neighbor_indices = jnp.argsort(dists)
+
+        def spin_cond_fn(value):
+            _, _, up, down = value
+            return (up + down < elec_of_atom).any()
+
+        def spin_body_fn(value):
+            i, center, up, down = value
+            is_down = (i % 2) & (down.sum() < self.mol.n_down)
+            up = up.at[center].add(1 - is_down)
+            down = down.at[center].add(is_down)
+            ordering = nearest_neighbor_indices[center]
+            ordered_has_remainder = (elec_of_atom - up - down)[ordering] > 0
+            first_ordered_has_remainder = jnp.argmax(ordered_has_remainder)
+            center = ordering[first_ordered_has_remainder]
+            return i + 1, center, up, down
+
+        center = argmax_random_choice(rng, elec_of_atom - up - down)
+        *_, up, down = lax.while_loop(
+            spin_cond_fn, spin_body_fn, (jnp.array(0), center, up, down)
+        )
+
         return up, down
 
     def local_energy(self, wf, return_grad=False):
