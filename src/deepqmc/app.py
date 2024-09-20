ed168b0fad30b3d1306e2c944cf8d8918971d27b
Zeno Schätzle <zenoone@physik.fu-berlin.de>
2024-09-20 11:01:39 +0200

    fix logging code version when not using git

--- src/deepqmc/app.py
+++ src/deepqmc/app.py
@@ -1,5 +1,6 @@
 import logging
 import platform
+import re
 import sys
 import warnings
 from pathlib import Path
@@ -139,17 +140,18 @@ def maybe_log_code_version():
     if log.isEnabledFor(logging.DEBUG):
         import subprocess
 
-        def git_command(command):
-            return (
-                subprocess.check_output(
-                    ['git'] + command, cwd=Path(__file__).resolve().parent
-                )
-                .strip()
-                .decode()
-            )
+        cwd = Path(__file__).resolve().parent
 
-        sha = git_command(['rev-parse', '--short', 'HEAD'])
-        diff = git_command(['diff'])
+        def git_command(command):
+            return subprocess.check_output(['git'] + command, cwd=cwd).strip().decode()
+
+        try:
+            sha = git_command(['rev-parse', '--short', 'HEAD'])
+            diff = git_command(['diff'])
+        except Exception:
+            with open(cwd.parent.parent / Path('pyproject.toml')) as f:
+                sha = 'deepqmc ' + re.findall("version = '([.\0-9]*)'", f.read())[0]
+            diff = None
         log.debug(f'Running with code version: {sha}')
         if diff:
             log.debug(f'With uncommitted changes:\n{diff}')
